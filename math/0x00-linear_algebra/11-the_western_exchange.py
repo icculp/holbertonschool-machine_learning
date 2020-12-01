1609c63108da2cf3c88681c9b9170979911213d8
Ian Culp <icculp@gmail.com>
2020-12-01 00:46:03 +0000

    Adding task 11

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+    Task 11
+"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """ transposes np array """
+    tranny = matrix.copy()
+    return tranny.transpose()
