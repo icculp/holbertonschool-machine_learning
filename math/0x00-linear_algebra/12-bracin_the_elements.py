323487e39fe0406b9a4ebb4099824ede6d575102
Ian Culp <icculp@gmail.com>
2020-12-01 00:59:30 +0000

    Adding 12

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Task 12
+"""
+import numpy as np
+
+
+def np_elementwise(mat1, mat2):
+    """ transposes np array """
+    sum = np.add(mat1, mat2)
+    dif = np.subtract(mat1, mat2)
+    prod = np.multiply(mat1, mat2)
+    quot = np.divide(mat1, mat2)
+    return (sum, dif, prod, quot)
