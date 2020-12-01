6a436489190f9ce3032c86a2fcac276c5621470a
Ian Culp <icculp@gmail.com>
2020-12-01 16:39:05 +0000

    Adding 13

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+    Task 13
+"""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """ concats matrices along a specific axis """
+    return np.concatenate((mat1, mat2), axis=axis)
