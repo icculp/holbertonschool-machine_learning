305fa194796359cc703918e6af3297eb6c97cc60
Ian Culp <icculp@gmail.com>
2020-12-02 14:06:32 +0000

    Fixing shallow copy for both matrices on 5

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -20,7 +20,8 @@ def add_matrices2D(mat1, mat2):
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
     new = list(map(list, mat1))
+    new2 = list(map(list, mat2))
     for i in range(len(mat1)):
         for j in range(len(mat1[0])):
-            new[i][j] += mat2[i][j]
+            new[i][j] += new2[i][j]
     return new
