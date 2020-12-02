efb60a00734f6bef45952c467b1a46583a7cf5c0
Ian Culp <icculp@gmail.com>
2020-12-02 22:02:58 +0000

    Adding files for task 16

---
+++ math/0x00-linear_algebra/101-the_whole_barn.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Task 16 advanced
+"""
+
+
+def matrix_shape(matrix):
+    """ Return shape as list of integers """
+    shape = []
+    if type(matrix) == int:
+        pass
+    else:
+        shape.append(len(matrix))
+        shape += matrix_shape(matrix[0])
+    return shape
+
+
+def add_matrices(mat1, mat2):
+    """ Adds two matrices """
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    return mat1 + mat2
