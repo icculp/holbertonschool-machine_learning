1b342b221ef4f382939a28ee9daeb5618407cdc4
Ian Culp <icculp@gmail.com>
2020-12-02 23:09:26 +0000

    Adding files for task 17

---
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+    Task 17 advanced
+"""
+
+
+def matrix_shape(matrix):
+    """ Return shape as list of integers """
+    shape = []
+    if type(matrix) != list:
+        pass
+    else:
+        shape.append(len(matrix))
+        shape += matrix_shape(matrix[0])
+    return shape
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    """ concatenates two matrices """
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
+    '''return [[w + u for w in v] for u, v in zip(mat1, mat2)] '''
+    if type(mat1[0]) != int:
+        return [cat_matrices(mat1[i], mat2[i]) for i in range(len(mat1))]
+    else:
+        return [mat1[i] + mat2[i] for i in range(len(mat1))]
