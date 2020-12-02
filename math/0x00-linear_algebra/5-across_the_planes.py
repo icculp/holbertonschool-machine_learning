7887b81a7e70272053c9bba525e5564389cb0028
Ian Culp <icculp@gmail.com>
2020-12-02 15:53:20 +0000

    Consolodating shape function into add function

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,23 +4,19 @@
 """
 
 
-def matrix_shape(matrix):
-    """ Return shape as list of integers """
-    shape = []
-    if type(matrix) != list:
-        pass
-    else:
-        shape.append(len(matrix))
-        shape += matrix_shape(matrix[0])
-    return shape
-
-
 def add_matrices2D(mat1, mat2):
     """ Adds two arrays of same size, else returns None """
     new = list(map(list, mat1))
     new2 = list(map(list, mat2))
-    if matrix_shape(new) != matrix_shape(new2):
+
+    try:
+        if len(mat1) != len(mat2):
+            return None
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+    except Exception:
         return None
+
     for i in range(len(mat1)):
         for j in range(len(new[0])):
             new[i][j] += new2[i][j]
