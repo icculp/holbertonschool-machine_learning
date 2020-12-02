477b5096ef58fcf39cdb9f5f6e31f8bd9fd70459
Ian Culp <icculp@gmail.com>
2020-12-02 17:36:29 +0000

    Matrix multiplication, cols in first must match rows in second

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -8,6 +8,8 @@ def mat_mul(mat1, mat2):
     """ Matrix multiplication (dot) """
     new = list(map(list, mat1))
     new2 = list(map(list, mat2))
+    if len(new[0]) != len(new2):
+        return None
     res = [[0] * len(new2[0]) for i in new]
     for i in range(len(new)):
         for j in range(len(new2[0])):
