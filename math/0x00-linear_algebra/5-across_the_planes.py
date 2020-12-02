2e411b6fc6fc1106497a4275adc2a59926d0beca
Ian Culp <icculp@gmail.com>
2020-12-02 15:45:17 +0000

    Accounting for floats, deep copy on input before typecheck

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -7,7 +7,7 @@
 def matrix_shape(matrix):
     """ Return shape as list of integers """
     shape = []
-    if type(matrix) == int:
+    if type(matrix) == int or type(matrix) == float:
         pass
     else:
         shape.append(len(matrix))
@@ -17,11 +17,11 @@ def matrix_shape(matrix):
 
 def add_matrices2D(mat1, mat2):
     """ Adds two arrays of same size, else returns None """
-    if matrix_shape(mat1) != matrix_shape(mat2):
-        return None
     new = list(map(list, mat1))
     new2 = list(map(list, mat2))
+    if matrix_shape(new) != matrix_shape(new2):
+        return None
     for i in range(len(mat1)):
-        for j in range(len(mat1[0])):
+        for j in range(len(new[0])):
             new[i][j] += new2[i][j]
     return new
