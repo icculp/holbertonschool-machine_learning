48d05261329bb84d7c253ab91afa2a9878f67bde
Ian Culp <icculp@gmail.com>
2020-12-10 03:21:17 +0000

    Removing test case

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -7,7 +7,6 @@
 def poly_derivative(poly):
     """ calculates the derivative of a polynomial """
     new = []
-    poly = [3, 3]
     if type(poly) is not list:
         return None
     for i in range(1, len(poly)):
