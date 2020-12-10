e2737bec64a77652139bfa241d6d37b3afaa2ae2
Ian Culp <icculp@gmail.com>
2020-12-10 17:22:33 +0000

    Task 17 complete, checking

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    Task 10
+"""
+
+
+def poly_integral(poly, C=0):
+    """ calculates the integral of a polynomial """
+    new = []
+    if type(poly) is not list or len(poly) == 0:
+        return None
+    if not all(isinstance(x, (int, float)) for x in poly)\
+            or not isinstance(C, (int, float)):
+        return None
+    new.append(C)
+    for i in range(len(poly)):
+        num = poly[i] / (i + 1)
+        if num.is_integer():
+            num = int(num)
+        new.append(num)
+    return new
