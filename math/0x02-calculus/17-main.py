ed6cad8cc138a0fd78c198a56409b4e5fa4df0cb
Ian Culp <icculp@gmail.com>
2020-12-10 17:29:04 +0000

    num() should've been int()

--- math/0x02-calculus/17-main.py
+++ math/0x02-calculus/17-main.py
@@ -4,3 +4,12 @@ poly_integral = __import__('17-integrate').poly_integral
 
 poly = [5, 3, 0, 1]
 print(poly_integral(poly))
+
+poly = [5, 3, 0, 1]
+print(poly_integral(poly, 1))
+
+poly = [5, 3, 0, 1]
+print(poly_integral(poly, 1.5))
+
+poly = [5, 3, 0, 1]
+print(poly_integral(poly, 1.0))
