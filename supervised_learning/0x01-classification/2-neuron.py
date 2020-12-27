8e8cddb30f2b23e6db9681967023b0e9ab938b78
Ian Culp <icculp@gmail.com>
2020-12-27 05:44:08 +0000

    Task 2 omplete

---
+++ supervised_learning/0x01-classification/2-neuron.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+    Build a neuron
+"""
+import numpy as np
+
+
+class Neuron():
+    """ Defines a single neuron performing binary classification """
+
+    def __init__(self, nx):
+        """ nx is number of input features
+            must be an integer and >= 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ W getter """
+        return self.__W
+
+    @property
+    def b(self):
+        """ W getter """
+        return self.__b
+
+    @property
+    def A(self):
+        """ W getter """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Forward propogation of the neuron """
+        aw = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-aw))
+        return self.__A
