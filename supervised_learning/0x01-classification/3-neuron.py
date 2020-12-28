aa99b67673d9a3b44d732d84eca569fa24ca47de
Ian Culp <icculp@gmail.com>
2020-12-28 16:13:47 +0000

    Cost function complete

---
+++ supervised_learning/0x01-classification/3-neuron.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+    Build a neuron
+"""
+import numpy as np
+
+
+class Neuron():
+    """ Defines a single neuron performing binary classification """
+
+    def __init__(self, nx):
+        """ nx is number of input features
+            must be an integer and >= 1
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """ W getter """
+        return self.__W
+
+    @property
+    def b(self):
+        """ W getter """
+        return self.__b
+
+    @property
+    def A(self):
+        """ W getter """
+        return self.__A
+
+    def forward_prop(self, X):
+        """ Forward propogation of the neuron """
+        aw = np.matmul(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-aw))
+        return self.__A
+
+    def cost(self, Y, A):
+        """ calculates the cost of the model using logistic regression """
+        from math import log
+        m = Y.shape[1]
+        loss = (Y * np.log(A)) + ((1.0000001 - Y) * np.log(1.0000001 - A))
+        cost = -(1 / m) * loss
+        return cost
