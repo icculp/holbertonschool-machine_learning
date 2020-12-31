c937d994c9620f08d62eed01f9c31219f10535a5
Ian Culp <icculp@gmail.com>
2020-12-31 05:15:27 +0000

    Adding attributes for nx and nodes

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -21,6 +21,8 @@ class NeuralNetwork:
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positve integer")
+        self.nx = nx
+        self.nodes = nodes
         self.W1 = np.random.randn(nodes, nx)
         '''self.b1 = np.array([np.array([0.]) for i in range(nodes)])'''
         self.b1 = np.zeros((nodes, 1))
