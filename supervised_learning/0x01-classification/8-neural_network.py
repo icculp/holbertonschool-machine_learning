a516b3e490035c7fadbcbe975773647d743f5203
Ian Culp <icculp@gmail.com>
2020-12-31 05:03:20 +0000

    Converting to np.array

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -21,8 +21,8 @@ class NeuralNetwork():
             raise TypeError("nodes must be an integer")
         if nodes < 1:
             raise ValueError("nodes must be a positve integer")
-        self.W1 = np.random.normal(size=(nodes, nx))
-        self.b1 = np.array([[0.] for i in range(nodes)])
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.array([np.array([0.]) for i in range(nodes)])
         self.A1 = 0
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
