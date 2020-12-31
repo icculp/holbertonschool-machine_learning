24cf59f8729519f50a0e3776278767ea6956677e
Ian Culp <icculp@gmail.com>
2020-12-31 05:13:11 +0000

    Trying np.zeros

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 
-class NeuralNetwork():
+class NeuralNetwork:
     """ Defines a neural network with one hidden layer
         performing binary classification """
 
@@ -22,8 +22,10 @@ class NeuralNetwork():
         if nodes < 1:
             raise ValueError("nodes must be a positve integer")
         self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.array([np.array([0.]) for i in range(nodes)])
+        '''self.b1 = np.array([np.array([0.]) for i in range(nodes)])'''
+        self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
+
         self.W2 = np.random.randn(1, nodes)
         self.b2 = 0
         self.A2 = 0
