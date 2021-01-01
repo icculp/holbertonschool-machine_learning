2694d3aeb82de396b2d8625e6f8febf840506764
Ian Culp <icculp@gmail.com>
2021-01-01 23:16:53 +0000

    Type checking for alpha

--- supervised_learning/0x01-classification/14-neural_network.py
+++ supervised_learning/0x01-classification/14-neural_network.py
@@ -107,6 +107,10 @@ class NeuralNetwork:
             raise TypeError("iterations must be an integer")
         if iterations <= 0:
             raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha <= 0:
+            raise ValueError("alpha must be positive")
         for i in range(iterations):
             A1, A2 = self.forward_prop(X)
             self.gradient_descent(X, Y, A1, A2, alpha)
