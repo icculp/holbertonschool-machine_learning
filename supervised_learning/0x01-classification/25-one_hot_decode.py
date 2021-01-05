a3ea2deeb20c1f9bf4c520f01a154da90261b7be
Ian Culp <icculp@gmail.com>
2021-01-05 00:48:36 +0000

    Another input check

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -8,6 +8,7 @@ import numpy as np
 def one_hot_decode(one_hot):
     """ Converts a one-hot matrix into a vector of labels """
     if type(one_hot) is not np.ndarray or\
+            len(one_hot.shape) != 2 or\
             one_hot.shape[0] < 1 or\
             one_hot.shape[1] < 1:
         return None
