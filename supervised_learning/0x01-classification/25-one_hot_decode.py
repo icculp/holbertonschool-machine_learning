e3dd669e5bb1b3d067a1d87b7caa9cab21212892
Ian Culp <icculp@gmail.com>
2021-01-05 00:47:09 +0000

    Another input check

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -8,7 +8,8 @@ import numpy as np
 def one_hot_decode(one_hot):
     """ Converts a one-hot matrix into a vector of labels """
     if type(one_hot) is not np.ndarray or\
-            one_hot.shape[0] < 1:
+            one_hot.shape[0] < 1 or\
+            one_hot.shape[1] < 1:
         return None
     try:
         hot = np.argmax(one_hot, axis=0)
