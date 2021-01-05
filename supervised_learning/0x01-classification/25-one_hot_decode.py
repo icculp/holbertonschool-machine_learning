e46cf135e155759a93c39e96d2675017f65f6b56
Ian Culp <icculp@gmail.com>
2021-01-05 00:29:36 +0000

    Removing check for ndarray

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -7,8 +7,8 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ Converts a one-hot matrix into a vector of labels """
-    if type(one_hot) is not np.ndarray:
-        return None
+    '''if type(one_hot) is not np.ndarray:
+        return None'''
     try:
         hot = np.argmax(one_hot, axis=0)
         return hot
