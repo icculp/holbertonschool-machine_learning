f551cf8757770a0849d38e40cc90ad7665db741d
Ian Culp <icculp@gmail.com>
2021-01-07 01:36:22 +0000

    layer without using keras

--- supervised_learning/0x02-tensorflow/1-create_layer.py
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -8,7 +8,6 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
     """ Returns tensor output of the layer """
     w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.keras.layers.Dense(n, activation=activation,
-                                  kernel_initializer=w, name='layer')
-    '''r = tf.keras.layers.Add(n, activation=activation, name='layer')(prev)'''
+    layer = tf.layers.Dense(n, activation=activation,
+                            kernel_initializer=w, name='layer')
     return layer(prev)
