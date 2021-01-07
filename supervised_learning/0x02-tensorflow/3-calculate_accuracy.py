ac49c3ec484b4a0dd33932f4fa3ecff9094ca623
Ian Culp <icculp@gmail.com>
2021-01-07 17:13:44 +0000

    Using convert_to_tensor

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -8,10 +8,8 @@ import tensorflow as tf
 def calculate_accuracy(y, y_pred):
     """ Calculates accuracy (cost) of a prediction """
     '''loss = tf.losses.softmax_cross_entropy('''
-    '''loss = tf.metrics.accuracy(
+    loss = tf.metrics.accuracy(
         y,
         y_pred, name="Meanie")
-    t = tf.metrics.mean(loss, name='Meaney')[0]
-    print(loss)
-    m = tf.convert_to_tensor(loss[0], name="Mean")'''
-    return tf.reduce_mean(((y - y_pred) ** 2), name="Mean")
+    m = tf.convert_to_tensor(loss[0], name="Mean")
+    return tf.reduce_mean(m, name="Mean")
