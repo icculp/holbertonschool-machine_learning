e2a9483fb4201503240d64d9138850e5c770897d
Ian Culp <icculp@gmail.com>
2021-01-08 05:46:24 +0000

    Evaluating saved model

---
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+    Tensorflow project
+"""
+import tensorflow as tf
+
+
+def evaluate(X, Y, save_path):
+    """
+        Evaluates output of a NN
+    """
+    _ = tf.Variable(initial_value='fake_variable')
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess, save_path)
+        x = tf.get_collection("x")[0]
+        y = tf.get_collecction("y")[0]
+        y_pred = tf.get_collection("y_pred")[0]
+        accuracy = tf.get_collection("accuracy")[0]
+        loss = tf.get_collection("y_pred")[0]
+        y_ = sess.run(y_pred, {x: X, y: Y})
+        ac = sess.run(accuracy, {x: X, y: Y})
+        loss = sess.run(loss, {x: X, y: Y})
+        return y_, ac, loss
