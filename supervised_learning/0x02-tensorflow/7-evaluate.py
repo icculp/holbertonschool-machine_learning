1bf5d6041f3ab3eedf30b3bae1f1c389379022f0
Ian Culp <icculp@gmail.com>
2021-01-08 05:59:17 +0000

    Importing meta graph

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -9,14 +9,15 @@ def evaluate(X, Y, save_path):
     """
         Evaluates output of a NN
     """
-    _ = tf.Variable(initial_value='fake_variable')
-    saver = tf.train.Saver()
+    '''_ = tf.Variable(initial_value='fake_variable')'''
     with tf.Session() as sess:
         all_variables = tf.get_collection_ref(tf.GraphKeys.GLOBAL_VARIABLES)
         sess.run(tf.variables_initializer(all_variables))
+        saver = tf.train.import_meta_graph("{}.meta".format(save_path))
+        '''saver = tf.train.Saver()'''
         saver.restore(sess, save_path)
         x = tf.get_collection("x")[0]
-        y = tf.get_collecction("y")[0]
+        y = tf.get_collection("y")[0]
         y_pred = tf.get_collection("y_pred")[0]
         accuracy = tf.get_collection("accuracy")[0]
         loss = tf.get_collection("loss")[0]
