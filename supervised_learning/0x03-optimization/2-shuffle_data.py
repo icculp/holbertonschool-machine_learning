4fc7449a694b024f5b7997b7b027291fab104eb7
Ian Culp <icculp@gmail.com>
2021-01-12 00:56:56 +0000

    Permuting two matrices the same

---
+++ supervised_learning/0x03-optimization/2-shuffle_data.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+    Optimization project
+"""
+import tensorflow as tf
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """ Shuffles data points in two matrices the same way
+        X is ndarray with shape (m nx)
+            m number of data points
+            nx number of features
+        Y is ndarray with shape (m nx)
+            m number of data points
+            nx number of features
+        Returns: shuffled X, Y matrices
+    """
+    permute = np.random.permutation(np.arange(X.shape[0]))
+    return X[permute], Y[permute]
