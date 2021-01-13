f7f5139ee586de21e5661364f69c9f09ab4af22d
Ian Culp <icculp@gmail.com>
2021-01-13 02:38:33 +0000

    Cost improving too quickly

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -36,16 +36,19 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
         saver = tf.train.import_meta_graph("{}.meta".format(load_path))
         saver.restore(sess, load_path)
         m = X_train.shape[0]
-        batches = int(m / batch_size)
+        batches = m / batch_size
+        if batches % 1 != 0:
+            batches = int(batches) + 1
+            
         x = tf.get_collection('x')[0]
         y = tf.get_collection('y')[0]
         accuracy = tf.get_collection('accuracy')[0]
         loss = tf.get_collection('loss')[0]
         train_op = tf.get_collection('train_op')[0]
         for i in range(epochs + 1):
-            
             shuf_x, shuf_y = shuffle_data(X_train, Y_train)
-            train_cost = loss.eval(feed_dict={x: X_train,
+
+            train_cost = loss.eval({x: X_train,
                                               y: Y_train})
             train_accuracy = accuracy.eval(feed_dict={x: X_train,
                                                       y: Y_train})
@@ -53,7 +56,6 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
                                               y: Y_valid})
             valid_accuracy = accuracy.eval(feed_dict={x: X_valid,
                                                       y: Y_valid})
-            """ before and after each epoch """
             print("After {} epochs:\n".format(i) +
                   "\tTraining Cost: {}\n".format(train_cost) +
                   "\tTraining Accuracy: {}\n".format(train_accuracy) +
@@ -61,15 +63,17 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid,
                   "\tValidation Accuracy: {}\n".format(valid_accuracy))
             if i == epochs:
                 ''' Done training, last epoch metrics printed '''
-                return saver.save(sess, saved_path)
+                return saver.save(sess, save_path)
             for j in range(batches):
                 start = batch_size * i
+                '''print("start", start)'''
                 end = batch_size * (i + 1)
+                '''print("end", end)'''
                 if end > m:
                     end = -1
                 sess.run(train_op, feed_dict={x: shuf_x[start:end],
                                               y: shuf_y[start:end]})
-                if j % 100 == 0:
+                if j % 100 == 0 and j != 0:
                     step_cost = loss.eval(feed_dict={x: shuf_x[start:end],
                                                       y: shuf_y[start:end]})
                     step_accuracy = accuracy.eval(feed_dict={x: shuf_x[start:end],
