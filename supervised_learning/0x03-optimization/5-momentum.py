f8f28061fd432e38875a9b60dd3dc2644de87ae8
Ian Culp <icculp@gmail.com>
2021-01-14 17:20:41 +0000

    Gradient descent with momentum optimization

---
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Optimization project
+"""
+import tensorflow as tf
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """ Updates tf variable using gradient descent w/ momentum
+        alpha is learning rate
+        beta1 is momentum weight
+        var is ndarray contaning var to be updated
+        grad contains gradient of var
+        v is previous first moment of var
+        Returns: updated variable and new moment
+    """
+    Vdw = beta1 * v + ((1 - beta1) * grad)
+    W = var - (alpha * Vdw)
+    return W, Vdw
