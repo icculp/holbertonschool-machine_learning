e0dd26cbf767116b359828a5e46d8aa72427b2ac
Ian Culp <icculp@gmail.com>
2021-01-14 22:11:12 +0000

    Learning rate decay by hand

---
+++ supervised_learning/0x03-optimization/11-learning_rate_decay.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+    Optimization project
+"""
+
+
+def learning_rate_decay(alpha, decay_rate,
+                        global_step, decay_step):
+    """ updates the learning rate in stepwise fasion
+        using inverse time decay in numpy
+        learning_rate_decay(alpha_init, 1, i, 10)
+
+        alpha is the original learning rate
+        decay_rate is the weight dermining alpha decay
+        global_step is number of gradient descent passes elapsed
+        decay_step is number of passes to occur before alpha decayed again
+
+        Returns: updated value for alpha
+    """
+    return alpha * (1 / (1 + decay_rate * (global_step // decay_step)))
+    '''return alpha * (decay_rate ** int(((1 + global_step) // decay_step)))'''
