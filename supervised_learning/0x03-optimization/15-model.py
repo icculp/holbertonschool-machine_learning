17feee226271f9a6e3144a0ce7528e3d9d4e3358
Ian Culp <icculp@gmail.com>
2021-01-16 02:44:16 +0000

    Executing but bad output

--- supervised_learning/0x03-optimization/15-model.py
+++ supervised_learning/0x03-optimization/15-model.py
@@ -2,43 +2,160 @@
 """
     Optimization project
 """
+import numpy as np
 import tensorflow as tf
 
 
 def model(Data_train, Data_valid, layers, activations,
           alpha=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8,
           decay_rate=1, batch_size=32, epochs=5, save_path='/tmp/model.ckpt'):
-    """ Shuffles data points in two matrices the same way
-        X_train is ndarray with shape (m, 784) containing traning data
-            m number of data points
-            784 is number of features
-        Y_train is ndarray with shape (m, 10) containing training labels
-            m number of data points
-            10 number of classes
-        X_valid (m, 784) contains validation data
-        Y_valid is one-hot (m, 10), validation labels
-        batch_size
-        epochs is # of times training passes through whole dataset
-        load_path/save_path where model stored
-        Returns: path where saved
+    """ Builds, trains, and saves a NN model in tf
+        using Adam optimzer, mini-batch gradient descent,
+        learning rate decay, and batch normalization
 
-        allow for smaller final batch
-        shuffle training data before each epoch
+        Data_train is tuple containing training input and labels
+        Data_valid is tuple containing validation input and labels
+        layers is list of nodes in each layer
+        activation is list of activatin functions for each layer
+        alpha is learning rate
+        beta1 is weight for first moment
+        beta2 is weight for second moment
+        epsilon to avoid div/0
+        decary_rate is rate for inverse time decay, step is 1
+        batch_size is number of data points in mini batch
+        epochs
+        Returns: path where saved
     """
+
+    def create_layer(prev, n, activation):
+        """ Returns tensor output of the layer """
+        w = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+        layer = tf.layers.Dense(n, activation=activation,
+                                kernel_initializer=w, name='layer')
+        return layer(prev)
+
+    def create_batch_norm_layer(prev, n, activation):
+        """ Creates a batch normalization layer
+            prev is activated output of previous layer
+            n is number of nodes
+            activation is activation function to be used
+            kernal initializer
+            tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+            gamma and beta initialized as vectors of 1, 0 respectively
+            epsilon of 1e-8
+            Returns: normlized Z matrix
+        """
+        k = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+        base = tf.layers.Dense(n, kernel_initializer=k)
+        mean, var = tf.nn.moments(base(prev), axes=[0])
+        gamma = tf.Variable(tf.ones([n]), trainable=True)
+        beta = tf.Variable(tf.zeros([n]), trainable=True)
+        epsilon = 1e-8
+        batch_norm = tf.nn.batch_normalization(base(prev), mean, var,
+                                               beta, gamma, epsilon)
+        return activation(batch_norm)
+
+    def forward_prop(x, layer_sizes=[], activation=[]):
+        """ creates forward propagation computation graph for NN """
+        inp = x
+        for i in range(len(layer_sizes)):
+            if activation[i] is None:
+                inp = create_layer(inp, layer_sizes[i], activation[i])
+            else:
+                inp = create_batch_norm_layer(inp, layer_sizes[i], activation[i])
+        return inp
+
+    def calculate_accuracy(y, y_pred):
+        """ Calculates accuracy (cost) of a prediction """
+        m = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
+        return tf.reduce_mean(tf.cast(m, tf.float32), name="Mean")
+
+    def calculate_loss(y, y_pred):
+        """ Calculates softmax cross-entropy loss of a prediction
+            y is placeholder for labels of input data
+            y_pred is tensor containing networks predictions
+        """
+        m = tf.losses.softmax_cross_entropy(
+            y,
+            y_pred)
+        return m
+
+    def create_train_op(loss, alpha):
+        """ Creates training operation for NN
+        """
+        grady = tf.train.GradientDescentOptimizer(alpha, name='GradientDescent')
+        return grady.minimize(loss)
+
+    def learning_rate_decay(alpha, decay_rate,
+                            global_step, decay_step):
+        """ creates learning rate decay operation in tensorflow
+            using inverse time decay
+            learning_rate_decay(alpha_init, 1, i, 10)
+            alpha is the original learning rate
+            decay_rate is the weight dermining alpha decay
+            global_step is number of gradient descent passes elapsed
+            decay_step is number of passes to occur before alpha decayed again
+            Returns: updated value for alpha
+        """
+        opt = tf.train.inverse_time_decay(alpha, global_step,
+                                          decay_step, decay_rate,
+                                          staircase=True)
+        return opt
+
+    def shuffle_data(X, Y):
+        """ Shuffles data points in two matrices the same way
+            X is ndarray with shape (m nx)
+                m number of data points
+                nx number of features
+            Y is ndarray with shape (m nx)
+                m number of data points
+                nx number of features
+            Returns: shuffled X, Y matrices
+        """
+        permute = np.random.permutation(X.shape[0])
+        return X[permute], Y[permute]
+
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph("{}.meta".format(load_path))
-        saver.restore(sess, load_path)
+        # splt data tuples into x, y
+        X_train = Data_train[0]
+        Y_train = Data_train[1]
+        X_valid = Data_valid[0]
+        Y_valid = Data_valid[1]
+        #create placeholders for tensorflow
+        x = tf.placeholder("float", shape=(None, X_train.shape[1]))
+        y = tf.placeholder("float", shape=(None, Y_train.shape[1]))
+        tf.add_to_collection('x', x)
+        tf.get_collection('y', y)
+        #m is examples I think?
         m = X_train.shape[0]
+        #setting batch size and manually implementing math.ciel
         batches = m / batch_size
         if batches % 1 != 0:
             batches = int(batches) + 1
         else:
             batches = int(batches)
-        x = tf.get_collection('x')[0]
-        y = tf.get_collection('y')[0]
-        accuracy = tf.get_collection('accuracy')[0]
-        loss = tf.get_collection('loss')[0]
-        train_op = tf.get_collection('train_op')[0]
+
+        #forward prop
+        ''' if activations none, modify fp '''
+        y_pred = forward_prop(x, layers, activations)
+        tf.add_to_collection('y_pred', y_pred)
+
+        accuracy = calculate_accuracy(y, y_pred)
+        tf.add_to_collection('accuracy', accuracy)
+
+        loss = calculate_loss(y, y_pred)
+        tf.add_to_collection('loss', loss)
+
+        ''' decay learning rate alpha '''
+        decay = learning_rate_decay(alpha, decay_rate, 0, 1)
+        train_op = create_train_op(loss, decay)
+
+
+        """ Initialize variables and run session """
+        init = tf.global_variables_initializer()
+        sess.run(init)
+
+        '''trainop pass into ? not alpha, but return of decay rate function'''
         for i in range(epochs + 1):
             train_cost = loss.eval({x: X_train,
                                     y: Y_train})
@@ -73,4 +190,5 @@ def model(Data_train, Data_valid, layers, activations,
                     print("\tStep {}:\n".format(j + 1) +
                           "\t\tCost: {}\n".format(step_cost) +
                           "\t\tAccuracy: {}".format(step_accuracy))
+        saver = tf.train.Saver()
         return saver.save(sess, save_path)
