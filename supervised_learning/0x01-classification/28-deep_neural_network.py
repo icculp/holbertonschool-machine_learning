c464d6c3fe771ffb7c16a6284987787eba60b55e
Ian Culp <icculp@gmail.com>
2021-01-18 20:23:27 +0000

    Modifying methods to account for diff activations

--- supervised_learning/0x01-classification/28-deep_neural_network.py
+++ supervised_learning/0x01-classification/28-deep_neural_network.py
@@ -10,7 +10,7 @@ class DeepNeuralNetwork:
     """ Defines a deep neural network
         performing binary classification """
 
-    def __init__(self, nx, layers):
+    def __init__(self, nx, layers, activation='sig'):
         """ nx is number of input features
             must be an integer and >= 1
         """
@@ -21,6 +21,8 @@ class DeepNeuralNetwork:
         if type(layers) is not list or len(layers) < 1:
             raise TypeError("layers must be a list of positive integers")
         self.__L = len(layers)
+        if activation not in ['sig', 'tanh']:
+            raise ValueError("activation must be 'sig' or 'tanh'")
 
         def checklist(li, i, L):
             """ checks if every element > 1 """
@@ -44,6 +46,11 @@ class DeepNeuralNetwork:
             self.__weights.update({w: np.random.randn(l1, l2)
                                   * np.sqrt(2 / l2)})
             self.__weights.update({b: np.zeros((l1, 1))})
+
+    @property
+    def activation(self):
+        """ activation getter """
+        return self.__activation
 
     @property
     def L(self):
@@ -71,14 +78,33 @@ class DeepNeuralNetwork:
             """ sigmoid activation function """
             return 1 / (1 + np.exp(-aw))
 
+        def tanh_act(aw):
+            """ tanh activation function """
+            return np.tanh(aw)
+
+        def soft_act(aw):
+            """ softmax activation function """
+            return np.exp(aw) / np.sum(np.exp(aw), axis=0)
+
         for i in range(self.L):
-            w = 'W' + str(i + 1)
-            a = 'A' + str(i)
-            b = 'b' + str(i + 1)
+            w = "W" + str(i + 1)
+            a = "A" + str(i)
+            b = "b" + str(i + 1)
             aw_ = np.matmul(self.__weights[w],
                             self.__cache[a]) + self.__weights[b]
-            A = 'A' + str(i + 1)
-            self.__cache[A] = sig_act(aw_)
+            A = "A" + str(i + 1)
+            if i == self.L - 1:
+                if self.activation == 'sig':
+                    act = sig_act(aw_)
+                if self.activation == 'tanh':
+                    act = tanh_act(aw_)
+                act = soft_act(aw_)
+            else:
+                if self.activation == 'sig':
+                    act = sig_act(aw_)
+                if self.activation == 'tanh':
+                    act = tanh_act(aw_)
+            self.__cache[A] = act
         return self.__cache[A], self.__cache
 
     def cost(self, Y, A):
@@ -87,8 +113,8 @@ class DeepNeuralNetwork:
             A contains the activated output for each example
         """
         m = Y.shape[1]
-        loss = np.sum((Y * np.log(A)) + ((1 - Y) * np.log(1.0000001 - A)))
-        cost = -(1 / m) * loss
+        loss = np.sum(-Y * np.log(A))
+        cost = loss / m
         return cost
 
     def evaluate(self, X, Y):
@@ -96,10 +122,43 @@ class DeepNeuralNetwork:
             X contains the input data (nx, m)
             Y contains the correct labels
         """
+        def one_hot_encode(Y, classes):
+            """ Converts numeric label vector into one-hot matrix """
+            if type(Y) is not np.ndarray:
+                return None
+            if type(classes) is not int:
+                return None
+            if len(Y.shape) != 1:
+                return None
+            try:
+                shape = (classes, Y.shape[0])
+                hot = np.eye(classes)[Y]
+                return hot.T
+            except Exception:
+                return None
+
+        def one_hot_decode(one_hot):
+            """ Converts a one-hot matrix into a vector of labels """
+            if type(one_hot) is not np.ndarray or\
+                    len(one_hot.shape) != 2 or\
+                    one_hot.shape[0] < 1 or\
+                    one_hot.shape[1] < 1:
+                return None
+            try:
+                hot = np.argmax(one_hot, axis=0)
+                return hot
+            except Exception:
+                return None
         a, _ = self.forward_prop(X)
         cost = self.cost(Y, a)
-        x = np.where(a >= 0.5, 1, 0)
-        return x, cost
+        """ decode predicted output
+            re-encode for every class
+        """
+        classes = Y.shape[0]
+        decoded = one_hot_decode(a)
+        encoded_classes = one_hot_encode(decoded, classes)
+        '''x = np.where(a >= 0.5, 1, 0)'''
+        return encoded_classes.astype(int), cost
 
     def gradient_descent(self, Y, cache, alpha=0.05):
         """ Calculates one pass of gradient descent on the NN
@@ -113,8 +172,12 @@ class DeepNeuralNetwork:
             if i == self.L:
                 dz = np.subtract(A_i, Y)
             else:
-                dz = np.matmul(wold['W' + str(i + 1)].T, dz2) *\
-                     np.multiply(A_i, (1 - A_i))
+                """ take derivative based on activation """
+                if self.activation == 'sig':
+                    der = np.multiply(A_i, (1 - A_i))
+                if self.activation == 'tanh':
+                    der = 1 - (A_i ** 2)
+                dz = np.matmul(wold['W' + str(i + 1)].T, dz2) * der
             dz2 = dz
             dw = np.matmul(dz, A_iless1.T) / m
             wupdate = np.subtract(wold['W' + str(i)], np.multiply(alpha, dw))
@@ -136,6 +199,11 @@ class DeepNeuralNetwork:
             raise TypeError("alpha must be a float")
         if alpha <= 0:
             raise ValueError("alpha must be positive")
+        if verbose is True or graph is True:
+            if type(step) is not int:
+                raise TypeError("step must be an integer")
+            if step < 1 or step > iterations:
+                raise ValueError("step must be positive and <= iterations")
         for i in range(iterations):
             A2, cache = self.forward_prop(X)
             if i == 0:
@@ -151,7 +219,6 @@ class DeepNeuralNetwork:
                 if verbose:
                     print("Cost after {} iterations: {}".format(i, c))
             self.gradient_descent(Y, cache, alpha)
-        i += 1
         if verbose:
             print("Cost after {} iterations: {}".format(i, c))
         if graph:
