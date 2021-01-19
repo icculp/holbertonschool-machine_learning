d0a21e8541fa190a6250611013ffbe5c6ec6163c
Ian Culp <icculp@gmail.com>
2021-01-19 22:18:35 +0000

    Precision for confusion matrix

---
+++ supervised_learning/0x04-error_analysis/2-precision.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Error Analysis project
+"""
+import numpy as np
+
+
+def precision(confusion):
+    """ calculates the precision for each class in confusion matrix
+        confusion is ndarray (classes, classes), rows correct coluns predicted
+        Returns: ndarray (classes,) containing sensitivity for each class
+    """
+    totals = sum(sum(label) for label in confusion)
+    '''print("totals", totals)'''
+    classes = len(confusion)
+    positives = np.sum(confusion, axis=0)
+    return [confusion[i][i] / positives[i] for i in range(classes)]
