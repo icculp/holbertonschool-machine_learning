25120f4ce5bbeb31d4b19fc4a21b1bedd26f0f41
Ian Culp <icculp@gmail.com>
2021-01-21 01:50:30 +0000

    Regularization project

---
+++ supervised_learning/0x04-error_analysis copy/0-l2_reg_cost.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    Regularization project
+"""
+
+
+def l2_reg_cost(cost, lambtha, weights, L, m):
+    """ Calculates the cost of a neural network w/ L2 regularization
+        cost is cost of NN w/o L2 regularization
+        lambtha is regularization parameter
+        weights is dict of weights and biases (ndarrays)
+        L is number of layers
+        m is number of data points
+        Returns: cost of NN accounting for L2 regularization
+    """
+    result = cost
+    return result
