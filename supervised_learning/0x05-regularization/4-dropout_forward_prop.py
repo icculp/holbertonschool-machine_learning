7abca949dc91775c9b3801a7251f2d8a5a98be4d
Ian Culp <icculp@gmail.com>
2021-01-25 01:33:04 +0000

    Forward prop with inverted dropout

--- supervised_learning/0x05-regularization/4-dropout_forward_prop.py
+++ supervised_learning/0x05-regularization/4-dropout_forward_prop.py
@@ -2,6 +2,7 @@
 """
     Regularization project
 """
+import numpy as np
 import tensorflow as tf
 
 
@@ -15,4 +16,31 @@ def dropout_forward_prop(X, weights, L, keep_prob):
         Returns: dict containing outputs of each layer and
         the dropout mask of each layer
     """
-    return
+    def tanh_act(aw):
+        """ tanh activation function """
+        return np.tanh(aw)
+
+    def soft_act(aw):
+        """ softmax activation function """
+        return np.exp(aw) / np.sum(np.exp(aw), axis=0)
+    cache = dict()
+    cache['A0'] = X
+    for i in range(L):
+        w = "W" + str(i + 1)
+        a = "A" + str(i)
+        b = "b" + str(i + 1)
+        d = "D" + str(i + 1)
+
+        aw_ = np.matmul(weights[w],
+                        cache[a]) + weights[b]
+        A = "A" + str(i + 1)
+        if i == (L - 1):
+            act = tanh_act(aw_)
+            act = soft_act(aw_)
+            cache[A] = act
+        else:
+            act = tanh_act(aw_)
+            cache[d] = np.random.rand(act.shape[0], act.shape[1])
+            cache[d] = np.where(cache[d] < keep_prob, 1, 0)
+            cache[A] = (1 / keep_prob) * np.multiply(act, cache[d])
+    return cache
