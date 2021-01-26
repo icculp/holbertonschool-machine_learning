72e10c85eed2d51159a8f8bc7a4bab448201c0c2
Ian Culp <icculp@gmail.com>
2021-01-26 16:37:53 +0000

    Optimizing network

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -16,4 +16,4 @@ def optimize_model(network, alpha, beta1, beta2):
         Returns: None
     """
     A = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
-    A(network)
+    network.compile(loss='categorical_crossentropy', optimizer=A)
