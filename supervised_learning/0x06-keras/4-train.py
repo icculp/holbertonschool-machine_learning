84551c0a53d72a5016329f2650b46a07c4c9bf5c
Ian Culp <icculp@gmail.com>
2021-01-26 17:44:21 +0000

    No need to compile

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -18,8 +18,8 @@ def train_model(network, data, labels, batch_size,
         shuffle is bool whether to shuffle at each epoch
         Returns: History object generated after training
     """
-    network.compile(loss='binary_crossentropy',
-                    optimizer='adam', metrics=['accuracy'])
+    '''network.compile(loss='binary_crossentropy',
+                    optimizer='adam', metrics=['accuracy'])'''
     '''print(dir(network))'''
     history = network.fit(data, labels, epochs=epochs, batch_size=batch_size,
                           verbose=verbose, shuffle=shuffle)
