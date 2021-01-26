0a0a5d449216df6538b1f54a16d0648551cf00b3
Ian Culp <icculp@gmail.com>
2021-01-26 19:08:51 +0000

    Pycode

--- supervised_learning/0x06-keras/3-one_hot.py
+++ supervised_learning/0x06-keras/3-one_hot.py
@@ -10,11 +10,4 @@ def one_hot(labels, classes=None):
         The last dimension of one-hot matrix must be number of classes
         Returns: One-hot matrix
     """
-    '''tf.compat.v1.enable_eager_execution()
-    print(type(labels))'''
-    '''oh = K.backend.one_hot(labels, labels.shape[-1])'''
-    '''if classes is None:
-        classes = labels.shape[-1]
-    oh = K.backend.one_hot(labels, classes)
-    return K.backend.get_session().run(oh)'''
     return K.utils.to_categorical(labels, classes)
