4d18a8cf41b2f3eb2c7ec0110015c62a50ffe196
Ian Culp <icculp@gmail.com>
2021-01-26 17:25:13 +0000

    testing train.py

--- supervised_learning/0x06-keras/4-train.py
+++ supervised_learning/0x06-keras/4-train.py
@@ -19,4 +19,7 @@ def train_model(network, data, labels, batch_size,
         shuffle is bool whether to shuffle at each epoch
         Returns: History object generated after training
     """
+    network.compile(loss='binary_crossentropy',
+                    optimizer='adam', metrics=['accuracy'])
+    network.fit(data, labels, epochs=epochs, batch_size=batch_size)
     return
