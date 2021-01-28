e6b22e06a58afa6c5537b39ef29b4e760d1b42bb
Ian Culp <icculp@gmail.com>
2021-01-28 15:24:10 +0000

    Checking but it's wrong

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -10,14 +10,15 @@ def convolve_grayscale_valid(images, kernel):
         two for loops allowed
         Returns: ndarray containing convoluted images
     """
-    out_h = images.shape[1] - kernel.shape[0] + 1
-    out_w = images.shape[2] - kernel.shape[1] + 1
     kh = kernel.shape[0]
     kw = kernel.shape[1]
+    out_h = images.shape[1] - kh + 1
+    out_w = images.shape[2] - kw + 1
     m = images.shape[0]
     output = np.zeros((m, out_h, out_w))
     for x in range(out_w):
         for y in range(out_h):
-            output[y, x] = np.sum(kernel * images[:, y:y + kh,
-                                  x:x + kw])
+            output[slice(0, m, 1), y, x] = (kernel *
+                                            images[slice(0, m, 1), y:y + kh,
+                                                   x:x + kw]).sum()
     return output
