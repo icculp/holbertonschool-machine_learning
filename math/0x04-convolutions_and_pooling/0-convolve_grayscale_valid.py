77c3c13fa6c5f77b4035e5aa07dadb170b20f222
Ian Culp <icculp@gmail.com>
2021-01-28 20:16:21 +0000

    Summing along axis 1 twice

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -16,9 +16,13 @@ def convolve_grayscale_valid(images, kernel):
     out_w = images.shape[2] - kw + 1
     m = images.shape[0]
     output = np.zeros((m, out_h, out_w))
+    '''print(kernel)'''
+    '''k = np.tile(kernel, (m, 1, 1))
+    k = np.stack(m * kernel)
+    k = kernel + np.zeros(m)'''
     for x in range(out_w):
         for y in range(out_h):
-            output[slice(0, m, 1), y, x] = (kernel *
-                                            images[slice(0, m, 1), y:y + kh,
-                                                   x:x + kw]).sum()
+            output[:, y, x] = (np.
+                               multiply(kernel, images[:, y:y + kh,
+                                        x:x + kw])).sum(axis=1).sum(axis=1)
     return output
