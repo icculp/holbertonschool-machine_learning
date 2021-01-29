ce8fa950bb1b8c11df240eb19a903b4445ea9680
Ian Culp <icculp@gmail.com>
2021-01-29 02:21:17 +0000

    Task 2

--- math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -5,32 +5,38 @@
 import numpy as np
 
 
-def convolve_grayscale_same(images, kernel):
-    """ Performs SAME convolution on grayscale
+def convolve_grayscale_padding(images, kernel, padding):
+    """ Performs custom padding convolution on grayscale
         images is ndarray (m, h, w) containing multiple images
         m # images, h height in pixels, w width in pixels
         kernel ndarray (kh, kw) kernel for convolution
         two for loops allowed
+        padding is a tuple of (ph, pw)
         Returns: ndarray containing convoluted images
     """
     input_h = images.shape[1]
     input_w = images.shape[2]
     kh = kernel.shape[0]
     kw = kernel.shape[1]
-    out_h = images.shape[1]
-    out_w = images.shape[2]
     m = images.shape[0]
 
-    """ Same = with padding, so have to create padded array """
+    padh = padding[0]
+    padw = padding[1]
+    out_h = images.shape[1] + kh + padh + 1
+    out_w = images.shape[2] - kw + padw + 1
+    '''print(out_h)
+    print(out_w)'''
     pad_along_height = max((out_h - 1) + kh - input_h, 0)
     pad_along_width = max((out_w - 1) + kw - input_w, 0)
     pad_top = pad_along_height // 2
     pad_bottom = pad_along_height - pad_top
     pad_left = pad_along_width // 2
     pad_right = pad_along_width - pad_left
+
     output = np.zeros((m, out_h, out_w))
-    image_padded = np.zeros((m, input_h + pad_along_height,
-                            input_w + pad_along_width))
+    image_padded = np.zeros((m, out_h + padh,
+                            out_w + padh))
+
     image_padded[:, pad_top:-pad_bottom, pad_left:-pad_right] = images
 
     for x in range(out_w):
