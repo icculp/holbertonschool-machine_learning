c40335e624718e2c868fa8585784ee72a9fbcb2a
Ian Culp <icculp@gmail.com>
2021-01-29 04:21:44 +0000

    Valid to use floor

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -20,16 +20,16 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     input_w = images.shape[2]
     m = images.shape[0]
     if padding == 'same':
-        out_h = images.shape[1] // stride[0]
-        out_w = images.shape[2] // stride[1]
+        out_h = int(images.shape[1] // stride[0])
+        out_w = int(images.shape[2] // stride[1])
         padh = int(kh / 2)
         padw = int(kw / 2)
         image_padded = np.pad(images, ((0, 0), (padh, padh),
                               (padw, padw)), 'constant',
                               constant_values=0)
     elif padding == 'valid':
-        out_h = (images.shape[1] - kh + 1) // stride[0]
-        out_w = (images.shape[2] - kw + 1) // stride[1]
+        out_h = int((images.shape[1] - kh + 1) // stride[0])
+        out_w = int((images.shape[2] - kw + 1) // stride[1])
         image_padded = images
     output = np.zeros((m, out_h, out_w))
     for x in range(out_w):
