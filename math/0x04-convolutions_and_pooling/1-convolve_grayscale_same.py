7646d16a464beb27050bd58fcd6ee2ac76ec9bb1
Ian Culp <icculp@gmail.com>
2021-01-29 03:15:45 +0000

    With constant_values

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -32,7 +32,8 @@ def convolve_grayscale_same(images, kernel):
                             input_w + pad_along_width))
     '''image_padded[:, pad_top:-pad_bottom, pad_left:-pad_right] = images'''
     image_padded = np.pad(images, ((0, 0), (pad_top, pad_bottom),
-                          (pad_left, pad_right)), 'edge')
+                          (pad_left, pad_right)), 'constant',
+                          constant_values=0)
     output = np.zeros((m, out_h, out_w))
     for x in range(out_w):
         for y in range(out_h):
