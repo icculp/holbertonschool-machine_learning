4f4fb33c3830c72d067d2a826f21e72a0e4f89d2
Ian Culp <icculp@gmail.com>
2021-01-29 04:18:32 +0000

    Accounting for strides in loop

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -3,7 +3,6 @@
     Convolutions project
 """
 import numpy as np
-from math import ceil, floor
 
 
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
@@ -21,21 +20,23 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     input_w = images.shape[2]
     m = images.shape[0]
     if padding == 'same':
-        out_h = int(ceil(float(input_h) / float(stride[0])))
-        out_w = int(ceil(float(input_w) / float(stride[1])))
+        out_h = images.shape[1] // stride[0]
+        out_w = images.shape[2] // stride[1]
         padh = int(kh / 2)
         padw = int(kw / 2)
         image_padded = np.pad(images, ((0, 0), (padh, padh),
                               (padw, padw)), 'constant',
                               constant_values=0)
     elif padding == 'valid':
-        out_h = int(ceil(float(input_h - kh + 1) / float(stride[0])))
-        out_w = int(ceil(float(input_w - kw + 1) / float(stride[1])))
+        out_h = (images.shape[1] - kh + 1) // stride[0]
+        out_w = (images.shape[2] - kw + 1) // stride[1]
         image_padded = images
     output = np.zeros((m, out_h, out_w))
     for x in range(out_w):
         for y in range(out_h):
+            ys = stride[0] * y
+            xs = stride[1] * x
             output[:, y, x] = (np.
-                               tensordot(image_padded[:, y:y + kh,
-                                         x:x + kw], kernel))
+                               tensordot(image_padded[:, ys:ys + kh,
+                                         xs:xs + kw], kernel))
     return output
