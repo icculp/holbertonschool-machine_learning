a2eedc94ce4ba7fc6115c200f2cb2e4ec43fd3b4
Ian Culp <icculp@gmail.com>
2021-01-29 00:00:07 +0000

    Cleaning comments and debugs

--- math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid copy.py
+++ math/0x04-convolutions_and_pooling/0-convolve_grayscale_valid copy.py
@@ -15,10 +15,27 @@ def convolve_grayscale_valid(images, kernel):
     out_h = images.shape[1] - kh + 1
     out_w = images.shape[2] - kw + 1
     m = images.shape[0]
-    output = np.zeros((m, out_h, out_w))
+    output = np.zeros((2, out_h, out_w))
+    '''#images = np.arrange(100)
+    #images.reshape()
+    A=np.array([[[1,2,3],[1,2,3],[1,2,3]],[[1,0,3],[1,9,3],[1,2,7]]])
+    print(kernel.shape)
+    k = kernel[0:2, 0:2]
+    print(k)
+    print(A)'''
+    '''print(kernel)'''
+    '''k = np.tile(kernel, (m, 1, 1))
+    k = np.stack(m * kernel)
+    k = kernel + np.zeros(m)'''
+    '''kh = 2
+    kw = 2
+    out_h = A.shape[1] - kh + 1
+    out_w = A.shape[2] - kw + 1
+    output = np.zeros((2, out_h, out_w))'''
     for x in range(out_w):
         for y in range(out_h):
             output[:, y, x] = (np.
                                tensordot(images[:, y:y + kh,
-                                         x:x + kw], kernel))
+                                        x:x + kw], k))
+    '''print(output)'''
     return output
