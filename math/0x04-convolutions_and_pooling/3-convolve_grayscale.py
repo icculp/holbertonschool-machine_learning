2911e4107b85935e19c63b34f088755632f28354
Ian Culp <icculp@gmail.com>
2021-01-29 05:06:37 +0000

    Good output

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -18,19 +18,22 @@ def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     kw = kernel.shape[1]
     input_h = images.shape[1]
     input_w = images.shape[2]
+
     m = images.shape[0]
     if padding == 'same':
-        out_h = int(images.shape[1] // stride[0])
-        out_w = int(images.shape[2] // stride[1])
-        padh = int(kh / 2)
-        padw = int(kw / 2)
+        '''padh = int(kh / 2) * stride[0]
+        padw = int(kw / 2) * stride[1]'''
+        padh = ((input_h - 1) * stride[0] + kh - input_h, 0) / 2
+        padw = ((input_w - 1) * strides[1] + k_w - input_w, 0) / 2
         image_padded = np.pad(images, ((0, 0), (padh, padh),
                               (padw, padw)), 'constant',
                               constant_values=0)
     elif padding == 'valid':
-        out_h = int((images.shape[1] - kh + 1) // stride[0])
-        out_w = int((images.shape[2] - kw + 1) // stride[1])
+        padh = 0
+        padw = 0
         image_padded = images
+    out_h = int(np.floor(images.shape[1] + 2 * padh - kh) // stride[0]) + 1
+    out_w = int(np.floor(images.shape[2] + 2 * padw - kw) // stride[1]) + 1
     output = np.zeros((m, out_h, out_w))
     for x in range(out_w):
         for y in range(out_h):
