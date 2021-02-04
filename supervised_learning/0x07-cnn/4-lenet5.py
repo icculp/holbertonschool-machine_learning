76cba2ac6ee175e02765ba76c4a069120f06ce3a
Ian Culp <icculp@gmail.com>
2021-02-04 17:42:59 +0000

    Flattening before dense layers

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -33,15 +33,15 @@ def lenet5(x, y):
                               padding='valid', activation='relu',
                               kernel_initializer=init)(layer2)
     layer4 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(layer3)
+    flat = tf.layers.Flatten()(layer4)
     layer5 = tf.layers.Dense(120, activation='relu',
-                             kernel_initializer=init)(layer4)
+                             kernel_initializer=init)(flat)
     layer6 = tf.layers.Dense(84, activation='relu',
                              kernel_initializer=init)(layer5)
-    flat = tf.layers.Flatten()(layer5)
     y_pred = tf.layers.Dense(10, activation='softmax',
-                             kernel_initializer=init)(flat)
+                             kernel_initializer=init)(layer6)
     loss = tf.losses.softmax_cross_entropy(y, y_pred)
-    grady = tf.train.AdamOptimizer(.001)
+    grady = tf.train.AdamOptimizer()
     op = grady.minimize(loss)
     acc = tf.equal(tf.argmax(y, 1), tf.argmax(y_pred, 1))
     accuracy = tf.reduce_mean(tf.cast(acc, tf.float32), name="Mean")
