0e0bd6c26b5813710f2a52e242b7a535b71f1fcc
Ian Culp <icculp@gmail.com>
2021-02-05 16:57:42 +0000

    Setting seed back to none

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -22,7 +22,7 @@ def lenet5(X):
             a tensor for the loss of the netowrk
             a tensor for the accuracy of the network
     """
-    init = K.initializers.he_normal(seed=none)
+    init = K.initializers.he_normal(seed=None)
     layer = K.layers.Conv2D(filters=6, kernel_size=(5, 5),
                             padding='same', activation='relu',
                             kernel_initializer=init,
