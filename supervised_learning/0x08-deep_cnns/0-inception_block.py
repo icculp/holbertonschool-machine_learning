98dcfe11a920bc684c165f84a95939ddbd3cf821
Ian Culp <icculp@gmail.com>
2021-02-09 20:04:59 +0000

    Inception block complete

---
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
+
+
+def inception_block(A_prev, filters):
+    """ Builds an inception block for deep CNN
+        A_prev is output from previous layer
+        filters tuple or list containing:
+            F1 # of filters in 1x1 conv
+            F3R # of filters in 1x1 before 3x3
+            F3 # filters in 3x3
+            F5R # filters in 1x1 before 5x5
+            F5 # filters in 5x5
+            FPP # filters in 1x1 after max pooling
+        ReLu use on all inside inception block
+        Returns: concatenated output of inception block
+    """
+    print(A_prev.shape)
+    F1, F3R, F3, F5R, F5, FPP = filters
+    one_conv1 = K.layers.Conv2D(F1, kernel_size=(1, 1),
+                                activation="relu", padding='same')(A_prev)
+    one_conv2 = K.layers.Conv2D(F3R, kernel_size=(1, 1),
+                                activation="relu", padding='same')(A_prev)
+    one_conv3 = K.layers.Conv2D(F5R, kernel_size=(1, 1),
+                                activation="relu", padding='same')(A_prev)
+    three_max_pool = K.layers.MaxPool2D(pool_size=(3, 3),
+                                        strides=(1, 1), padding='same')(A_prev)
+    three_conv = K.layers.Conv2D(F3, kernel_size=(3, 3),
+                                 activation="relu", padding='same')(one_conv2)
+    five_conv = K.layers.Conv2D(F5, kernel_size=(5, 5),
+                                activation="relu", padding='same')(one_conv3)
+    one_conv4 = K.layers.Conv2D(FPP, kernel_size=(1, 1),
+                                activation="relu",
+                                padding='same')(three_max_pool)
+    out = K.layers.Concatenate()([one_conv1, three_conv, five_conv, one_conv4])
+    return out
---
+++ supervised_learning/0x08-deep_cnns/README.md
@@ -0,0 +1,10 @@
+# 0x08. Deep Convolutional Architectures
+* What is a skip connection?
+* What is a bottleneck layer?
+* What is the Inception Network?
+* What is ResNet? ResNeXt? DenseNet?
+* How to replicate a network architecture by reading a journal article
+* Vanishing Gradients
+* GoogleNet
+* Highway Networks
+* Deep Residual Learning for Image Recognition
