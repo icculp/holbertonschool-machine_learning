87e02d145971cd2f399d0a60419716ad9e1c8130
Ian Culp <icculp@gmail.com>
2021-02-10 15:51:10 +0000

    Removing debug print

---
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import tensorflow.keras as K
+inception_block = __import__('0-inception_block').inception_block
+
+
+def inception_network():
+    """ Builds an inception network model
+        Returns: keras model
+    """
+    x = K.Input(shape=(224, 224, 3))
+    one_conv1 = K.layers.Conv2D(F5R, kernel_size=(7, 7),
+                                strides=(2, 2),
+                                activation="relu", padding='same')(x)
+    mpool = K.layers.MaxPool2D(pool_size=(3, 3),
+                               strides=(2, 2), padding='same')(one_conv1)
+    one = inception_block(x, [64, 96, 128, 16, 32, 32])
+    two = inception_block(one, [64, 96, 128, 16, 32, 32])
+    three = inception_block(two, [64, 96, 128, 16, 32, 32])
+    model = K.Model(inputs=x, outputs=three)
+    return model
