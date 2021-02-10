ed953e20f260feb4c56dc8f392835faf5ec3e5aa
Ian Culp <icculp@gmail.com>
2021-02-10 16:27:55 +0000

    Adding layers in order to match intranet

--- supervised_learning/0x08-deep_cnns/0-inception_block.py
+++ supervised_learning/0x08-deep_cnns/0-inception_block.py
@@ -19,20 +19,25 @@ def inception_block(A_prev, filters):
         Returns: concatenated output of inception block
     """
     F1, F3R, F3, F5R, F5, FPP = filters
-    one_conv3 = K.layers.Conv2D(F5R, kernel_size=(1, 1),
+
+    one_conv1 = K.layers.Conv2D(F1, kernel_size=(1, 1),
                                 activation="relu", padding='same')(A_prev)
+
     one_conv2 = K.layers.Conv2D(F3R, kernel_size=(1, 1),
                                 activation="relu", padding='same')(A_prev)
     three_conv = K.layers.Conv2D(F3, kernel_size=(3, 3),
                                  activation="relu", padding='same')(one_conv2)
-    three_max_pool = K.layers.MaxPool2D(pool_size=(3, 3),
-                                        strides=(1, 1), padding='same')(A_prev)
+
+    one_conv3 = K.layers.Conv2D(F5R, kernel_size=(1, 1),
+                                activation="relu", padding='same')(A_prev)
     five_conv = K.layers.Conv2D(F5, kernel_size=(5, 5),
                                 activation="relu", padding='same')(one_conv3)
+
+    three_max_pool = K.layers.MaxPool2D(pool_size=(3, 3),
+                                        strides=(1, 1), padding='same')(A_prev)
     one_conv4 = K.layers.Conv2D(FPP, kernel_size=(1, 1),
                                 activation="relu",
                                 padding='same')(three_max_pool)
-    one_conv1 = K.layers.Conv2D(F1, kernel_size=(1, 1),
-                                activation="relu", padding='same')(A_prev)
+
     out = K.layers.Concatenate()([one_conv1, three_conv, five_conv, one_conv4])
     return out
