cbbe1890c60a71b459ad4806d35b8b7dc9e04768
Ian Culp <icculp@gmail.com>
2021-02-19 03:28:20 +0000

    Restartign

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -17,7 +17,7 @@ def preprocess_data(X, Y):
         
     """
     #X_p = X.reshape(X.shape[0], 229, 229, X.shape[3])
-    #factor = 299 / 
+    #factor = 299 /
     resize = K.layers.Lambda(lambda X: K.backend.resize_images(X, 9 , 9, 'channels_last'), input_shape=(32, 32, 3))
     flat = K.layers.Flatten()
     base = K.applications.inception_v3.InceptionV3(input_shape=(288,288,3), weights='imagenet', include_top=False)#(include_top=True, weights='imagenet')#, input_shape=(32,32,3))#, input_shape=X.shape)
@@ -36,11 +36,7 @@ def preprocess_data(X, Y):
     adam = K.optimizers.Adam()
     model.compile(optimizer=adam, loss='categorical_crossentropy',
                   metrics=['accuracy'])
-    #model.summary()
     model.fit(X_p, Y_p)#X_p, steps_per_epoch=1)#X_p, steps_per_epoch=1, epochs=1)
-    #model.summary()
     model.save('cifar10.h5')
     return X_p, Y_p
-
-if __name__ == '__main__':
-    pass
+%tensorflow_version 1.x
