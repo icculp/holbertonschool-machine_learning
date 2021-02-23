4f56a3960628f7df27c1b1664268db4174e6ed6d
Ian Culp <icculp@gmail.com>
2021-02-23 16:36:41 +0000

    Object detection skeletong

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -14,29 +14,63 @@ def preprocess_data(X, Y):
             Y_p ndarray containing preprocessed X
         model must exceed 87% validation accuracy
         Save trained model as ./cifar10.h5
-        
     """
-    #X_p = X.reshape(X.shape[0], 229, 229, X.shape[3])
-    #factor = 299 /
-    resize = K.layers.Lambda(lambda X: K.backend.resize_images(X, 9 , 9, 'channels_last'), input_shape=(32, 32, 3))
-    flat = K.layers.Flatten()
-    base = K.applications.inception_v3.InceptionV3(input_shape=(288,288,3), weights='imagenet', include_top=False)#(include_top=True, weights='imagenet')#, input_shape=(32,32,3))#, input_shape=X.shape)
+    X_p = K.applications.inception_resnet_v2.preprocess_input(X)
+    Y_p = K.utils.to_categorical(Y)
+    return X_p, Y_p
+
+
+if __name__ == '__main__':
+    import tensorflow as tf
+    (X_train, Y_train), (X_test, Y_test) = K.datasets.cifar10.load_data()
+    X_train_p, Y_train_p = preprocess_data(X_train, Y_train)
+    X_test_p, Y_test_p = preprocess_data(X_test, Y_test)
+    '''resize = K.layers.Lambda(lambda X:
+    K.backend.resize_images(X, 9 , 9,
+    'channels_last'), input_shape=(32, 32, 3))'''
+    resize = K.layers.Lambda(lambda X:
+                             tf.image.resize_images(X, (299, 299)),
+                             input_shape=(32, 32, 3))
+    '''tf.image.resize(image, (299, 299))
+    #flat = K.layers.Flatten()'''
+    base = K.applications.InceptionResNetV2(input_shape=(299, 299, 3),
+                                            weights='imagenet',
+                                            include_top=False)
+    '''#(include_top=True, weights='imagenet')#,
+    input_shape=(32,32,3))#, input_shape=X.shape)'''
+    for layer in base.layers:
+        layer.trainable = False
     model = K.Sequential()
     model.add(resize)
     model.add(base)
-    model.add(flat)
-    model.add(K.layers.Dense(10, activation="softmax"))
-    #model.add(resize)
+    '''#print(len(base.layers))
+    #model.add(flat)'''
+    model.add(K.layers.GlobalAveragePooling2D())
+    '''#model.add(K.layers.BatchNormalization())'''
+    model.add(K.layers.Dense(512, activation="relu"))
+    model.add(K.layers.Dropout(.3))
 
-    #X_p = X / 255
-    X_p = X# / 255 #K.applications.inception_v3.preprocess_data(X)
-    Y_p = K.utils.to_categorical(Y)
-    #X_p = K.applications.resnet50.preprocess_input(X)
+    '''model.add(K.layers.BatchNormalization())
+    model.add(K.layers.Dense(256, activation="relu"))
+    model.add(K.layers.Dropout(.2))
+
+    #model.add(K.layers.BatchNormalization())
+    model.add(K.layers.Dense(128, activation="relu"))
+    model.add(K.layers.Dropout(.2))'''
+
+    '''#model.add(K.layers.BatchNormalization())'''
+    model.add(K.layers.Dense(10, activation="softmax"))
+
+    '''#check = K.callbacks.ModelCheckpoint('cifar102.h5',
+    save_best_only=True, monitor='val_loss')'''
+
     model.summary()
     adam = K.optimizers.Adam()
+    '''#rms = K.optimizers.RMSprop()'''
     model.compile(optimizer=adam, loss='categorical_crossentropy',
-                  metrics=['accuracy'])
-    model.fit(X_p, Y_p)#X_p, steps_per_epoch=1)#X_p, steps_per_epoch=1, epochs=1)
+                  metrics=['acc'])
+
+    model.fit(X_train_p, Y_train_p, epochs=10,
+              batch_size=32, shuffle=True,
+              validation_data=(X_test_p, Y_test_p))
     model.save('cifar10.h5')
-    return X_p, Y_p
-%tensorflow_version 1.x
