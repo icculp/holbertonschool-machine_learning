96beb628918c44eae5a14bb01cf6e19db5f5763d
Ian Culp <icculp@gmail.com>
2021-03-07 22:40:09 +0000

    NST class constructor

--- supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -5,27 +5,31 @@
 """
 import numpy as np
 import tensorflow as tf
+tf.enable_eager_execution()
 
 
 class NST():
     """ Neural style transfer class """
-    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1', 'block4_conv1', 'block5_conv1']
+    style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
+                    'block4_conv1', 'block5_conv1']
     content_layer = 'block5_conv2'
 
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """ class constructor """
-        if type(style_image) is not np.ndarray or
-                style_image.shape[2] != 3 or
-                style_image.ndmin != 3:
-            raise TypeError('style_image must be a numpy.ndarray with shape (h, w, 3)')
-        if type(content_image) is not np.ndarray or
-                style_image.shape[2] != 3 or
-                style_image.ndmin != 3:
-            raise TypeError('content_image must be a numpy.ndarray with shape (h, w, 3)')
-        if type(alpha) is not in [int, float] or
+        if type(style_image) is not np.ndarray or\
+                style_image.shape[2] != 3 or\
+                style_image.ndim != 3:
+            raise TypeError('style_image must be a' +
+                            ' numpy.ndarray with shape (h, w, 3)')
+        if type(content_image) is not np.ndarray or\
+                style_image.shape[2] != 3 or\
+                style_image.ndim != 3:
+            raise TypeError('content_image must be a' +
+                            ' numpy.ndarray with shape (h, w, 3)')
+        if type(alpha) not in [int, float] or\
                 alpha < 0:
             raise TypeError('alpha must be a non-negative number')
-        if type(beta) is not in [int, float] or
+        if type(beta) not in [int, float] or\
                 beta < 0:
             raise TypeError('beta must be a non-negative number')
         self.style_image = self.scale_image(style_image)
@@ -35,4 +39,19 @@ class NST():
 
     @staticmethod
     def scale_image(image):
-        """ scales the image to 
+        """ scales image dimensions and values to 0-1 """
+        if type(image) is not np.ndarray or\
+                image.shape[2] != 3 or\
+                image.ndim != 3:
+            raise TypeError('image must be a ' +
+                            'numpy.ndarray with shape (h, w, 3)')
+        h, w, _ = image.shape
+        max_dim = 512
+        maximum = max(h, w)
+        scale = max_dim / maximum
+        new_shape = (int(h * scale), int(w * scale))
+        image = np.expand_dims(image, axis=0)
+        scaled_image = tf.image.resize_bicubic(image, new_shape)
+        scaled_image = tf.clip_by_value(scaled_image / 255, 0, 1)
+
+        return scaled_image
