85c82d9c645f4f492329ab8fc7418fcb1d04b80f
Ian Culp <icculp@gmail.com>
2021-03-08 00:48:05 +0000

    Updating load_model method with changes in constructor

--- supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -7,7 +7,7 @@ import numpy as np
 import tensorflow as tf
 
 
-class NST():
+class NST:
     """ Neural style transfer class """
     style_layers = ['block1_conv1', 'block2_conv1', 'block3_conv1',
                     'block4_conv1', 'block5_conv1']
@@ -16,15 +16,23 @@ class NST():
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """ class constructor """
         if type(style_image) is not np.ndarray or\
-                style_image.shape[2] != 3 or\
-                style_image.ndim != 3:
+                style_image.ndim != 3 or\
+                style_image.shape[2] != 3:
             raise TypeError('style_image must be a' +
                             ' numpy.ndarray with shape (h, w, 3)')
         if type(content_image) is not np.ndarray or\
-                style_image.shape[2] != 3 or\
-                style_image.ndim != 3:
+                content_image.ndim != 3 or\
+                content_image.shape[2] != 3:
             raise TypeError('content_image must be a' +
                             ' numpy.ndarray with shape (h, w, 3)')
+        style_h, style_w, style_c = style_image.shape
+        content_h, content_w, content_c = content_image.shape
+        if style_h <= 0 or style_w <= 0 or style_c != 3:
+            raise TypeError(
+                "style_image must be a numpy.ndarray with shape (h, w, 3)")
+        if content_h <= 0 or content_w <= 0 or content_c != 3:
+            raise TypeError(
+                "content_image must be a numpy.ndarray with shape (h, w, 3)")
         if type(alpha) not in [int, float] or\
                 alpha < 0:
             raise TypeError('alpha must be a non-negative number')
@@ -42,15 +50,22 @@ class NST():
     def scale_image(image):
         """ scales image dimensions and values to 0-1 """
         if type(image) is not np.ndarray or\
-                image.shape[2] != 3 or\
-                image.ndim != 3:
+                image.ndim != 3 or\
+                image.shape[2] != 3:
             raise TypeError('image must be a ' +
                             'numpy.ndarray with shape (h, w, 3)')
+        h, w, c = image.shape
+        if h <= 0 or w <= 0 or c != 3:
+            raise TypeError(
+                "image must be a numpy.ndarray with shape (h, w, 3)")
         h, w, _ = image.shape
-        max_dim = 512
-        maximum = max(h, w)
-        scale = max_dim / maximum
-        new_shape = (int(h * scale), int(w * scale))
+        if h > w:
+            h_new = 512
+            w_new = int(w * (512 / h))
+        else:
+            w_new = 512
+            h_new = int(h * (512 / w))
+        new_shape = (h_new, w_new)
         image = np.expand_dims(image, axis=0)
         scaled_image = tf.image.resize_bicubic(image, new_shape)
         scaled_image = tf.clip_by_value(scaled_image / 255, 0, 1)
