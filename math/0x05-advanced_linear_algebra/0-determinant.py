3c7f7daffb31ad93e29b731250b1f4f65c6da63d
Ian Culp <icculp@gmail.com>
2021-03-15 22:22:21 +0000

    Determinant calculator complete

---
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+    Advanced Linear Algebra
+
+    not allowed to import any module
+    must be done by hand!
+"""
+
+
+def determinant(matrix):
+    """ Calculates the determinant of a matrix
+        matrix is a square list of lists whose determinant should be calculated
+        Returns: the determinant of matrix
+    """
+    ''' copy list beyond axis0'''
+    if matrix == [[]]:
+        return 1
+    if type(matrix) is not list or len(matrix) < 1 or\
+            not all(isinstance(x, list) for x in matrix):
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) != len(matrix[0]):
+        raise TypeError("matrix must be a square matrix")
+    copy = list(map(list, matrix))
+    dim = len(matrix)
+    if dim == 1:
+        return matrix[0][0]
+    elif dim == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+    else:
+        for cur in range(dim):
+            for i in range(cur + 1, dim):
+                if copy[cur][cur] == 0:
+                    copy[cur][cur] = 1.0e-18
+                curScaler = copy[i][cur] / copy[cur][cur]
+                for j in range(dim):
+                    copy[i][j] = copy[i][j] - curScaler * copy[cur][j]
+        det = 1.0
+        for i in range(dim):
+            det *= copy[i][i]
+    return int(det)
---
+++ math/0x05-advanced_linear_algebra/README.md
@@ -0,0 +1,6 @@
+# 0x05. Advanced Linear Algebra
+* What is a determinant? How would you calculate it?
+* What is a minor, cofactor, adjugate? How would calculate them?
+* What is an inverse? How would you calculate it?
+* What are eigenvalues and eigenvectors? How would you calculate them?
+* What is definiteness of a matrix? How would you determine a matrix’s definiteness?
