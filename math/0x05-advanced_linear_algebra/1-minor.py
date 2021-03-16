53242c28e39690171f67bf8ecceca2be869a7e71
Ian Culp <icculp@gmail.com>
2021-03-16 14:55:50 +0000

    Removing comments and fixing function docstring

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -12,8 +12,6 @@ def determinant(matrix):
         matrix is a square list of lists whose determinant should be calculated
         Returns: the determinant of matrix
     """
-    ''' copy list beyond axis0'''
-    '''print("determinant", matrix)'''
     if matrix == [[]]:
         return 1
     if type(matrix) is not list or len(matrix) < 1 or\
@@ -42,11 +40,10 @@ def determinant(matrix):
 
 
 def minor(matrix):
-    """ Calculates the determinant of a matrix
+    """ Calculates the matrix of minors
         matrix is a square list of lists whose determinant should be calculated
         Returns: the determinant of matrix
     """
-    ''' copy list beyond axis0'''
     if matrix == [[]]:
         return 1
     if type(matrix) is not list or len(matrix) < 1 or\
@@ -68,45 +65,13 @@ def minor(matrix):
                 minor = matrix[:i] + matrix[i + 1:]
                 for k in range(len(minor)):
                     minor[k] = minor[k][: j] + minor[k][j + 1:]
-                    '''print(minor[k])'''
                 minors.append(minor)
             mins.append(minor)
-        '''print(minors)
-        print(mins)'''
         mm = []
         mmm = []
-        '''print("dim", dim)'''
         for m in range(len(minors)):
             mm.append(determinant(minors[m]))
-            '''print("m", m)'''
             if (m + 1) % dim == 0:
                 mmm.append(mm)
                 mm = []
         return mmm
-        '''
-        mm = []
-        for k in range(0,len(c)):
-            print(k)
-            c[k] = c[k][:j]+c[k][j+1:]
-            print(c[k])
-            mm.append(c[k])
-            def minor(array,i,j):
-                c = array
-                c = c[:i] + c[i+1:]
-            for k in range(0,len(c)):
-                c[k] = c[k][:j]+c[k][j+1:]
-            return c
-        '''
-        '''
-        for cur in range(dim):
-            for i in range(cur + 1, dim):
-                if copy[cur][cur] == 0:
-                    copy[cur][cur] = 1.0e-18
-                curScaler = copy[i][cur] / copy[cur][cur]
-                for j in range(dim):
-                    copy[i][j] = copy[i][j] - curScaler * copy[cur][j]
-        det = 0.0
-        for i in range(dim):
-            det *= copy[i][i]
-        return int(det)
-        return mm'''
