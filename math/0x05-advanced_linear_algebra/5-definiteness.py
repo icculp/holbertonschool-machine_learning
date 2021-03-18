c8e771e14d5c727a01d952779339545481b7ab43
Ian Culp <icculp@gmail.com>
2021-03-18 01:50:47 +0000

    Definiteness checker complete

--- math/0x05-advanced_linear_algebra/5-definiteness.py
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -2,102 +2,37 @@
 """
     Advanced Linear Algebra
 
-    not allowed to import any module
+    not allowed to im-port any module
     must be done by hand!
 """
 
 
-def determinant(matrix):
-    """ Calculates the determinant of a matrix
-        matrix is a square list of lists whose determinant should be calculated
-        Returns: the determinant of matrix
+def definiteness(matrix):
+    """ Calculates definiteness of a matrix
+        matrix is a ndarray (n, n) whose definiteness should be calculated
+        Return: the string: Positive definite,
+                            Positive semi-definite,
+                            Negative semi-definite,
+                            Negative definite, or
+                            Indefinite
     """
-    if matrix == [[]]:
-        return 1
-    if type(matrix) is not list or len(matrix) < 1 or\
-            not all(isinstance(x, list) for x in matrix):
-        raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]):
-        raise TypeError("matrix must be a square matrix")
-    copy = list(map(list, matrix))
-    dim = len(matrix)
-    if dim == 1:
-        return matrix[0][0]
-    elif dim == 2:
-        return matrix[0][0] * matrix[1][1] - matrix[1][0] * matrix[0][1]
+    import numpy as np
+    if type(matrix) is not np.ndarray:
+        raise TypeError("matrix must be a numpy.ndarray")
+
+    if len(matrix) >= 1 and np.array_equal(matrix, matrix.T):
+        eigvals = np.linalg.eigvals(matrix)
+        '''print(matrix)
+        print("eigvals", eigvals)'''
+        if np.all(eigvals > 0):
+            return "Positive definite"
+        elif np.all(eigvals >= 0):
+            return "Positive semi-definite"
+        elif np.all(eigvals < 0):
+            return "Negative definite"
+        elif np.all(eigvals <= 0):
+            return "Negative semi-definite"
+        else:
+            return "Indefinite"
     else:
-        for cur in range(dim):
-            for i in range(cur + 1, dim):
-                if copy[cur][cur] == 0:
-                    copy[cur][cur] = 1.0e-18
-                curScaler = copy[i][cur] / copy[cur][cur]
-                for j in range(dim):
-                    copy[i][j] = copy[i][j] - curScaler * copy[cur][j]
-        det = 1.0
-        for i in range(dim):
-            det *= copy[i][i]
-    return int(det)
-
-
-def cofactor(matrix):
-    """ Calculates the matrix of cofactors
-        matrix is a square list of lists whose determinant should be calculated
-        Returns: the determinant of matrix
-    """
-    if matrix == [[]]:
-        return 1
-    if type(matrix) is not list or len(matrix) < 1 or\
-            not all(isinstance(x, list) for x in matrix):
-        raise TypeError("matrix must be a list of lists")
-    if len(matrix) != len(matrix[0]):
-        raise TypeError("matrix must be a non-empty square matrix")
-    copy = list(map(list, matrix))
-    dim = len(matrix)
-    if dim == 1:
-        return [[1]]
-    elif dim == 2:
-        return [[matrix[1][1], matrix[1][0]], [matrix[0][1], matrix[0][0]]]
-    else:
-        minors = []
-        mins = []
-        for i in range(dim):
-            for j in range(dim):
-                minor = matrix[:i] + matrix[i + 1:]
-                for k in range(len(minor)):
-                    minor[k] = minor[k][: j] + minor[k][j + 1:]
-                minors.append(minor)
-            mins.append(minor)
-        mm = []
-        mmm = []
-        for m in range(len(minors)):
-            co = 1 if ((m + 1) % 2) else -1
-            mm.append(co * determinant(minors[m]))
-            if (m + 1) % dim == 0:
-                mmm.append(mm)
-                mm = []
-        return mmm
-
-
-def adjugate(matrix):
-    """ basically just transpose the minor matrix """
-    mm = cofactor(matrix)
-    adj = list(zip(*mm))
-    adj = [list(x) for x in adj]
-    return adj
-
-
-def inverse(matrix):
-    """ calculates the inverse
-        basically multiply (adjucate) * (1 / determinant)
-        Returns: the inverse of matrix, or None if matrix is singular
-    """
-    adj = adjugate(matrix)
-    det = determinant(matrix)
-    if det == 0:
         return None
-    dim = len(matrix)
-    inverse = list(map(list, matrix))
-    for i in range(dim):
-        for j in range(dim):
-            inverse[i][j] = (adj[i][j]) * (1 / det)
-    return inverse
