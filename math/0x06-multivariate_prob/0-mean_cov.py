c5977ee23eab806d8387193c523f1d35fac50e6d
Ian Culp <icculp@gmail.com>
2021-03-22 17:38:50 +0000

    Project dir and task 0

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+    Multivariate Probability
+
+    only numpy allowed
+"""
+import numpy as np
+
+
+def mean_cov(X):
+    """ Calculates the mean and covariance of a dataset
+        X ndarray (n, d) n # data points, d # dimensions
+        Returns: mean, cov
+            (1, d), (d, d)
+
+        NOT ALLOWED numpy.cov
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
+    if n < 2:
+        raise ValueError("X must contain multiple data points")
+    '''print(X)
+    print(X.shape)'''
+    mean = np.mean(X, axis=0)
+    ones = np.ones((n, 1))
+    '''#x = X - ones.T @ ones @ ((ones.T @ ones) ** -1)
+    #x = (x.T @ x) / n'''
+    x = X - (ones @ ones.T @ X) * (1 / n)
+    x = x.T @ x
+    x = x * (1 / n)
+    return mean, x
