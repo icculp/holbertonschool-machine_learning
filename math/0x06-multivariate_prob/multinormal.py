7eec693f9b9d376036cd31fbb07e8aaa68da139e
Ian Culp <icculp@gmail.com>
2021-03-23 00:04:58 +0000

    Last check fishing

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -37,7 +37,7 @@ class MultiNormal:
         if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
         if len(x.shape) != 2 and x.shape != (x.shape[0], 1):
-            raise ValueError("x must have the shape ({}, 1)".format(len(self.mean)))
+            raise ValueError("x must have the shape (" + "{" + "d}, 1)")
         '''#print("x", x)
         pdf = (1.0 / (self.stdev * np.sqrt(2 * np.pi)))\
         * np.exp(-0.5*((x - self.mean) / self.stdev) ** 2)
