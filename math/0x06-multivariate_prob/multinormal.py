4c50a99bff8107ad6273dfeff6619fc497949fe2
Ian Culp <icculp@gmail.com>
2021-03-23 02:31:16 +0000

    Testing x.shape for a peer

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -36,7 +36,7 @@ class MultiNormal:
         """
         if type(x) is not np.ndarray:
             raise TypeError("x must be a numpy.ndarray")
-        d = len(self.mean)
+        d = self.cov.shape[0]
         if len(x.shape) != 2 or x.shape != (d, 1):
             raise ValueError('x must have the shape ({}, 1)'.format(d))
         '''#print("x", x)
