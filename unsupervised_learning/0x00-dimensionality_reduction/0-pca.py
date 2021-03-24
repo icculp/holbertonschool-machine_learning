44cc466950fb93cbde185b3b955b391d90ac85e2
Ian Culp <icculp@gmail.com>
2021-03-24 14:36:14 +0000

    Pycodestyle

--- unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/0-pca.py
@@ -10,31 +10,22 @@ import numpy as np
 
 
 def standardize_data(X):
-         
     '''
-    This function standardize an array, its substracts mean value, 
+    This function standardize an array, its substracts mean value,
     and then divide the standard deviation.
-    
-    param 1: array 
+    param 1: array
     return: standardized array
-    '''    
+    '''
     rows, columns = X.shape
-    
     standardizedArray = np.zeros(shape=(rows, columns))
     tempArray = np.zeros(rows)
-    
     for column in range(columns):
-        
-        mean = np.mean(X[:,column])
-        std = np.std(X[:,column])
+        mean = np.mean(X[:, column])
+        std = np.std(X[:, column])
         tempArray = np.empty(0)
-        
-        for element in X[:,column]:
-            
-            tempArray = np.append(tempArray, ((element - mean) / std))
- 
-        standardizedArray[:,column] = tempArray
-    
+        for element in X[:, column]:
+            tempArray = np.append(tempArray, ((element) / std))
+        standardizedArray[:, column] = tempArray
     return standardizedArray
 
 
@@ -48,15 +39,15 @@ def pca(X, var=0.95):
         Returns: weights matrix, W ndarray (d, nd)
             nd new dims that transformed X
     """
-    #print("xshape", X.shape)
+    '''#print("xshape", X.shape)
     #mean = np.mean(X.T)
-    #print("mean", mean)
+    #print("mean", mean)'''
     s = np.std(X)
-    #mean = np.expand_dims(mean, axis=0)
-    #print("S", s)
+    '''#mean = np.expand_dims(mean, axis=0)
+    #print("S", s)'''
     n = X.shape[0]
-    #X = X - mean
     X = X / s
+    '''#X = standardize_data(X)
     print("XXXXXXXXXXXXXXX", X)
     #x = standardize_data(X)
     #cov = np.cov(x.T)
@@ -65,32 +56,32 @@ def pca(X, var=0.95):
     #print("mshape", M.shape)
     #C = X - M
     #print("cshape", C.shape)
-    V = np.cov(X.T)# / (n - 1)
-    #V = X.T * X * (1 / (n - 1))
-    print("Vshape", V.shape)
+    #V = X.T * X * (1 / (n - 1))'''
+    V = np.cov(X.T)  # / (n - 1)
+    '''print("Vshape", V.shape)'''
     values, vectors = np.linalg.eig(V)
-    print("values", values.shape)
-    print('vectors', vectors.shape)
-    #P = vectors.T.dot(C.T)
+    '''print("values", values.shape)
+    print('vectors', vectors.shape)'''
+    '''#P = vectors.T.dot(C.T)'''
     idx = values.argsort()[::-1]
     values = values[idx]
     vectors = vectors[:, idx]
     variance_explained = []
     for i in values:
         variance_explained.append((i / sum(values)))
-    print("variance_explained", variance_explained)
+    '''print("variance_explained", variance_explained)'''
     cum = np.cumsum(variance_explained)
     for c in range(len(cum)):
         if cum[c] > var:
             break
     c += 1
     projection_matrix = (vectors.T[:][:c + 1]).T
-    print("cum", cum)
-    #W = x * x.T
+    '''print("cum", cum)'''
+    '''#W = x * x.T
     #T = X @ W
     #print(T)
     #print(len(W))
     #print(W.shape)
     #pca = X.dot(projection_matrix)
-    #print(pca.shape)
+    #print(pca.shape)'''
     return projection_matrix * -1.0
