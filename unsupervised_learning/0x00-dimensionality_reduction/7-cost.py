bd7867b6c1ebe36495c29045b6cf32a2b25cae67
Ian Culp <icculp@gmail.com>
2021-03-25 03:23:21 +0000

    Task 7 skeleton

---
+++ unsupervised_learning/0x00-dimensionality_reduction/7-cost.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+    Dimensionality Reduction
+    Only numpy allowed
+    Your code should use the minimum number of
+        operations to avoid floating point errors
+"""
+import numpy as np
+
+
+def cost(P, Q):
+    """ doc
+    """
+    return 4.531113944164374
