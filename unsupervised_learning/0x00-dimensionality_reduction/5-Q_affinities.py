7b54ea1d78c706510dcf1a528d344fa92241583a
Ian Culp <icculp@gmail.com>
2021-03-25 03:19:46 +0000

    Task 5 skeleton

---
+++ unsupervised_learning/0x00-dimensionality_reduction/5-Q_affinities.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+    Dimensionality Reduction
+    Only numpy allowed
+    Your code should use the minimum number of
+        operations to avoid floating point errors
+"""
+import numpy as np
+
+
+def Q_affinities(Y):
+    """ doc
+    """
+    Q = np.ndarray((X.shape[0], X.shape[0]))
+    num = np.ndarray((X.shape[0], X.shape[0]))
+    return Q, num
