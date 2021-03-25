c590542618f6052847036a19d6782042161e8f06
Ian Culp <icculp@gmail.com>
2021-03-25 03:16:37 +0000

    Task 4 skeleton

---
+++ unsupervised_learning/0x00-dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+    Dimensionality Reduction
+    Only numpy allowed
+    Your code should use the minimum number of
+        operations to avoid floating point errors
+"""
+import numpy as np
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """ calculates Shannon entropy and P affinities relative to data point:
+        Di ndarray (n - 1,)  pariwise distances between
+            a data point and all other points except itself
+        n is the number of data points
+        beta ndarray (1,) beta value for the Gaussian distribution
+        Returns: (Hi, Pi)
+            Hi: the Shannon entropy of the points
+            Pi: ndarray of shape (n - 1,)
+                containing the P affinities of the points
+    """
+    return np.ndarray((X.shape[0], X.shape[0]))
