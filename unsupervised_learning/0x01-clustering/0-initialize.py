c98c7b442352d192f29c58b6d5480fe5479776c9
Ian Culp <icculp@gmail.com>
2021-03-30 01:10:46 +0000

    Clusters initialized

---
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+    Clustering
+"""
+import numpy as np
+
+
+def initialize(X, k):
+    """ Initialized cluster centroids for K-means
+        X ndarray (n, d) dataset to cluster
+        n # data points, d # dimensions
+        k positive int # clusters
+        Initialized with multivariate uniform distribution
+            along each dimension in d
+        Min values for distribution min of X along each dim
+        Same for max
+        No loops allowed
+        Returns: ndarray (k, d) initialized centroids for each cluster
+            or None on failure
+    """
+    n, d = X.shape
+    init = np.random.uniform(low=X.min(axis=0),
+                             high=X.max(axis=0),
+                             size=(k, d))
+    return init
