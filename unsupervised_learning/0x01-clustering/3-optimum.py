e17b66c2eb431e7a45cff1a654112dfa8d48d497
Ian Culp <icculp@gmail.com>
2021-03-31 15:34:25 +0000

    Fishing

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -2,8 +2,6 @@
 """
 Tests for the optimum number of clusters by variance
 """
-
-
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 variance = __import__('2-variance').variance
@@ -11,25 +9,39 @@ variance = __import__('2-variance').variance
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     """
-    Returns: results, d_vars, or None, None on failure
+    Function that tests for the optimum number of clusters by variance
+    Arguments:
+     - X is a numpy.ndarray of shape (n, d) containing the data set
+     - kmin is a positive integer containing the minimum number of clusters
+        to check for (inclusive)
+     - kmax is a positive integer containing the maximum number of clusters
+        to check for (inclusive)
+     - iterations is a positive integer containing the maximum number of
+        iterations for K-means
+    Returns:
+     results, d_vars, or None, None on failure
+        - results is a list containing the outputs of K-means for each cluster
+            size
+        - d_vars is a list containing the difference in variance from the
+            smallest cluster size for each cluster size
     """
-    if type(X) is not np.ndarray or len(X.shape) != 2:
+
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None
 
     if kmax is None:
         kmax = X.shape[0]
+
+    if type(kmin) != int or kmin <= 0 or kmin >= X.shape[0]:
         return None, None
 
-    if type(kmin) is not int or kmin <= 0 or X.shape[0] <= kmin:
+    if type(kmax) != int or kmax <= 0 or kmax > X.shape[0]:
         return None, None
 
-    if type(kmax) is not int or kmax <= 0 or X.shape[0] < kmax:
+    if kmin >= kmax:
         return None, None
 
-    if kmax <= kmin:
-        return None, None
-
-    if type(iterations) is not int or iterations <= 0:
+    if type(iterations) != int or iterations <= 0:
         return None, None
 
     results = []
@@ -38,11 +50,9 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     for k in range(kmin, kmax + 1):
         C, clss = kmeans(X, k, iterations)
         results.append((C, clss))
-
         if k == kmin:
-            max_var = variance(X, C)
+            var_min = variance(X, C)
 
-        total_var = variance(X, C)
-        d_vars.append(max_var - total_var)
+        d_vars.append(var_min - variance(X, C))
 
     return results, d_vars
