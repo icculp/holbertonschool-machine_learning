d8fc1e862ff5f617c654aa14342845193d9909c7
Ian Culp <icculp@gmail.com>
2021-03-31 16:14:23 +0000

    Fishing - checking task 3

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -1,37 +1,31 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
-Created on Mon Mar 25 08:33:12 2021
-@author: Robinson Montes
+    Clustering
 """
 import numpy as np
 
 
 def variance(X, C):
+    """ Calculates the total intra-cluster variance
+        X ndarray (n, d) dataset to cluster
+            n # data points, d # dimensions
+        C ndarray (k, d) centroid means for each cluster
+            k # data points, d # dimensions
+        Returns: var, or None on failure
+            var is the total variance
     """
-    Function that calculates the total intra-cluster variance for a data set
-    Arguments:
-     - X is a numpy.ndarray of shape (n, d) containing the data set
-     - C is a numpy.ndarray of shape (k, d) containing the centroid means
-        for each cluster
-    Returns:
-     var, or None on failure
-         - var is the total variance
-    """
-
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if type(X) is not np.ndarray or X.ndim != 2\
+            or type(C) is not np.ndarray or C.ndim != 2:
         return None
-
-    if not isinstance(C, np.ndarray) or len(X.shape) != 2:
-        return None
-
     try:
-        n, d = X.shape
-        distance = np.sqrt(((X - C[:, np.newaxis])**2).sum(axis=-1))
-        min_distance = np.min(distance, axis=0)
-        v = np.sum(min_distance ** 2)
-
-        return v
-
+        k, d = C.shape
+        # print("cshape", C.shape)
+        '''n, _ = X.shape
+        k, d = C.shape'''
+        distances = np.linalg.norm(X - np.expand_dims(C, 1), axis=-1)
+        # print(distances.shape)
+        min = np.min(distances, axis=0)
+        # print(min)
+        return np.sum(np.square(min))
     except Exception:
         return None
