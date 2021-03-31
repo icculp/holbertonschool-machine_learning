3dbe9cd61fa8592ab285fd131b7b90081736af15
Ian Culp <icculp@gmail.com>
2021-03-31 02:14:26 +0000

    Adding try/except git push

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -17,12 +17,15 @@ def variance(X, C):
     if type(X) is not np.ndarray or X.ndim != 2\
             or type(C) is not np.ndarray or C.ndim != 2:
         return None
-    k, d = C.shape
-    # print("cshape", C.shape)
-    '''n, _ = X.shape
-    k, d = C.shape'''
-    distances = np.linalg.norm(X - np.expand_dims(C, 1), axis=2)
-    # print(distances.shape)
-    min = np.min(distances, axis=0)
-    # print(min)
-    return np.sum(np.square(min))
+    try:
+        k, d = C.shape
+        # print("cshape", C.shape)
+        '''n, _ = X.shape
+        k, d = C.shape'''
+        distances = np.linalg.norm(X - np.expand_dims(C, 1), axis=2)
+        # print(distances.shape)
+        min = np.min(distances, axis=0)
+        # print(min)
+        return np.sum(np.square(min))
+    except:
+        return None
