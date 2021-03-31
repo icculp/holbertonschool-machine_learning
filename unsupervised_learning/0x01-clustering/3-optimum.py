82c59bdaee1853b7e8348f928054776e48005a2e
Ian Culp <icculp@gmail.com>
2021-03-31 16:04:38 +0000

    Fishing

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Tests for the optimum number of clusters by variance
+    Clustering
 """
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
@@ -8,8 +8,29 @@ variance = __import__('2-variance').variance
 
 
 def optimum_k(X, kmin=1, kmax=None, iterations=1000):
-    """Program that tests for the optimum number of
-    clusters by variance"""
+    """ tests for the optimum number of clusters by variance
+        X ndarray (n, d) dataset to cluster
+            n # data points, d # dimensions
+        kmin positive int min # of clusters to check for (inclusive)
+        kmax positive int max # of clusters to check for (inclusive)
+        iterations positive int
+        This function should analyze at least 2 different cluster sizes
+        Returns: results, d_vars, or None, None on failure
+            results list outputs of K-means for each cluster size
+            d_vars list difference in variance from the
+                smallest cluster size for each cluster size
+    """
+    '''if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None
+    n, d = X.shape
+    if kmax is None:
+        kmax = n
+    # or kmin >= n or kmax > n
+    if (kmax <= kmin)\
+            or type(kmin) is not int or kmin < 1\
+            or type(kmax) is not int or kmax < 1\
+            or type(iterations) is not int or iterations < 1:
+        return None, None'''
     if type(X) is not np.ndarray or len(X.shape) != 2:
         return None, None
     if kmax is None:
@@ -25,8 +46,11 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
     results = []
     d_vars = []
     for k in range(kmin, kmax + 1):
-        C, clss = kmeans(X, k, iterations)
-        results.append((C, clss))
-        var = variance(X, results[0][0]) - variance(X, C)
-        d_vars.append(var)
-    return results, d_vars
+        centroids, clss = kmeans(X, k, iterations=iterations)
+        var = variance(X, centroids)
+        if k == kmin:
+            mini = var
+        results.append((centroids, clss))
+        d_vars.append(mini - var)
+    # maxi = max(d_vars)
+    return results, d_vars  # maxi - d_vars
