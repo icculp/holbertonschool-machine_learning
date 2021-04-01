7cd870d2823bce231d0e8035a6f9da78c679d627
Ian Culp <icculp@gmail.com>
2021-04-01 02:55:00 +0000

    PDF finally

---
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+    Clustering
+"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def pdf(X, m, S):
+    """ probability density function of a Gaussian distribution
+        X ndarray (n, d) dataset
+            n # data points to evaluate pdf, d # dimensions
+        m ndarray (d,) mean of distribution
+        S ndarray (d, d) covariance of distribution
+        No loops
+        no n-p d-iag functions allowed
+        Returns: P, or None on failure
+            P ndarray (n,) containing the PDF values for each data point
+            All values in P should have a minimum value of 1e-300
+    """
+    print(X.shape)
+    # print(k)
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None
+    n, d = X.shape
+
+    det = np.linalg.det(S)
+    # print(det)
+    xm = X - m
+    print("xm shape", xm.shape)
+    # print(xm)
+    # inv = np.linalg.inv(S)
+    # print("inv shape", inv.shape)
+
+    xm = X - m
+    norm = 1 / (np.power(2 * np.pi, (d / 2)) * np.sqrt(det))
+    inv = np.linalg.inv(S)
+    res = np.exp(-0.5 * (xm @ inv @ xm.T))
+    P = (norm * res)  # [0] # [0]
+    P = P.reshape(len(P) ** 2)[::len(P) + 1]
+    return np.maximum(P, 1e-300)
+    """
+    norm = 1 / (2 * np.pi) ** (d / 2) * det
+    res = np.exp(-.5 * (xm @ inv @ xm.T))
+    # maha = np.sum(np.square(np.dot(xm, inv)), axis=-1)"""
+    P = (norm * res).diag()  # [0][0]
+    P = P.reshape(len(P) ** 2)[::len(P) + 1]
+    P = P.reshape(len(P) ** 2)[::len(P) + 1]
+    return P
