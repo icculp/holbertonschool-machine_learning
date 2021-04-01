55e73d2332ca15b8a132af4baf8549ed3483d74c
Ian Culp <icculp@gmail.com>
2021-04-01 19:35:13 +0000

    Trying felipe's conditionals

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -17,7 +17,7 @@ def pdf(X, m, S):
             P ndarray (n,) containing the PDF values for each data point
             All values in P should have a minimum value of 1e-300
     """
-
+    """
     if type(X) is not np.ndarray or X.ndim != 2\
             or type(m) is not np.ndarray or m.ndim != 1\
             or type(S) is not np.ndarray or S.ndim != 2:
@@ -25,6 +25,23 @@ def pdf(X, m, S):
     n, d = X.shape
     if d != m.shape[0] or d != S.shape[0]\
             or S.shape[0] != S.shape[1] or d != S.shape[1]:
+        return None
+    """
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+
+    if type(m) is not np.ndarray or len(m.shape) != 1:
+        return None
+
+    if type(S) is not np.ndarray or len(S.shape) != 2:
+        return None
+
+    d = X.shape[1]
+
+    if d != m.shape[0] or d != S.shape[0]:
+        return None
+
+    if S.shape[0] != S.shape[1] or d != S.shape[1]:
         return None
     det = np.linalg.det(S)
     # xm = X - m
