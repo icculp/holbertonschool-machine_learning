f257c03b97c873b0e3eaacd6ff04c34a15b66753
Ian Culp <icculp@gmail.com>
2021-04-07 17:51:02 +0000

    Expanding dims to match output shape expectation

--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -23,4 +23,20 @@ def regular(P):
     q = np.c_[q, ones]
     QTQ = np.dot(q, q.T)
     bQT = np.ones(dim)
-    return np.expand_dims(np.linalg.solve(QTQ, bQT), axis=0)
+    return np.linalg.solve(QTQ, bQT)
+
+
+def absorbing(P):
+    """ determines if a markov chain is absorbing:
+        P square 2D ndarray (n, n) transition matrix
+            P[i, j] is the probability of transitioning from state i to state j
+            n is the number of states in the markov chain
+        Returns: True if it is absorbing, or False on failure
+    """
+    if type(P) is not np.ndarray or P.shape[0] != P.shape[1]:
+        return None
+    '''if not (P > 0).all():
+        return False'''
+    if np.any(np.diag(P) == 1) and regular(P) != None:
+        return True
+    return False
