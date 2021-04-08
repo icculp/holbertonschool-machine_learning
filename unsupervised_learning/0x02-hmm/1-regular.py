a135fae8dfaea6ff18190021c3ec991bbaa54f0f
Ian Culp <icculp@gmail.com>
2021-04-08 21:49:31 +0000

    Sigh

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -24,3 +24,19 @@ def regular(P):
     QTQ = np.dot(q, q.T)
     bQT = np.ones(dim)
     return np.expand_dims(np.linalg.solve(QTQ, bQT), axis=0)
+
+''' Alternative from Mohammed
+def regular(P):
+  n, m = np.shape(P)
+  if (P == np.eye(n)).all():
+    return None
+  a, b = np.linalg.eig(P.T)
+  l = []
+  for i in range(len(a)):
+    if np.allclose(a[i], 1):
+      l.append(i)
+  if len(l) == 1:
+    return np.abs(b[:, l[0]].T)/np.sum(np.abs(b[:, l[0]].T))
+  else:
+    return None
+'''
--- unsupervised_learning/0x02-hmm/2-absorbing.py
+++ unsupervised_learning/0x02-hmm/2-absorbing.py
@@ -5,25 +5,25 @@
 import numpy as np
 
 
-def regular(P):
-    """ determines the steady state probabilities of a regular markov chain
-        P square 2D ndarray (n, n) transition matrix
-            P[i, j] is the probability of transitioning from state i to state j
-            n is the number of states in the markov chain
-        Returns: ndarray (1, n) containing the steady state
-            probabilities, or None on failure
-    """
-    if type(P) is not np.ndarray or P.shape[0] != P.shape[1]:
-        return None
-    if not (P > 0).all():
-        return None
-    dim = P.shape[0]
-    q = (P - np.eye(dim))
-    ones = np.ones(dim)
-    q = np.c_[q, ones]
-    QTQ = np.dot(q, q.T)
-    bQT = np.ones(dim)
-    return np.linalg.solve(QTQ, bQT)
+def lock(boxes):
+    ''' lockbox method to determine if edges connected
+            to absorbing state
+    '''
+    length = len(boxes)
+    open = [0 for i in range(length)]
+    open[0] = 1
+    s = [0]
+    while s:
+        n = s.pop()
+        for i in boxes[n]:
+            try:
+                if not open[i]:
+                    open[i] = 1
+                    s.append(i)
+            except IndexError:
+                pass
+    os = sum(open)
+    return True if os == length else False
 
 
 def absorbing(P):
@@ -33,10 +33,70 @@ def absorbing(P):
             n is the number of states in the markov chain
         Returns: True if it is absorbing, or False on failure
     """
-    if type(P) is not np.ndarray or P.shape[0] != P.shape[1]:
-        return None
-    '''if not (P > 0).all():
-        return False'''
-    if np.any(np.diag(P) == 1) and regular(P) != None:
+    if type(P) is not np.ndarray or\
+            len(P.shape) != 2 or \
+            P.shape[0] != P.shape[1]:
+        return False
+
+    # print("1", P == 1)
+    # print("2", np.any(P == 1, axis=0))
+    # print(P[:, np.any(P==1, axis=1)])
+    # print(np.argwhere(P != 0))
+    # print(np.where(P != 0))
+    # box = []
+    # for p in P:
+    #    box.append(p.nonzero())
+
+    # print(np.any(P!=0, axis=1))
+    # print(np.nonzero(P))
+    # print(lock(P[:, np.any(P==1, axis=0)] ))
+    # return
+    diag = np.diag(P)
+    # dim = P.shape[0]
+    '''q = (P - np.eye(dim))
+    ones = np.ones(dim)'''
+    # print("q", q)
+    '''q = np.c_[q, ones]
+    QTQ = np.dot(q, q.T)
+    # print(QTQ)
+    bQT = np.ones(dim)'''
+    # print("bQT", bQT)
+    # print("solved", np.linalg.solve(QTQ, bQT))
+    # print(diag)
+    if np.any(diag == 1):  # and regular(P) == None:
+        # try:
+        # Q, R = decompose(P)
+        # print("q", Q, "R", R)
+        '''if np.all(np.sum(Q, axis=0)) == True:
+            #n = expected_steps_fast(Q)
+            #print(n)
+            #print(96)
+            return True'''
+        # d, v = np.linalg.eig(P)
+        # s = np.sum(v @ np.diag(d >= 1).astype(int)
+        # @ np.linalg.inv(v), axis=1)
+        # print(s)
+        # if (s == 1).all():
+        #    # print(96)
+        #    return True
+        box = []
+        for p in P:
+            box.append(np.argwhere(p != 0).tolist()[0])
+        b = lock(box)
+        if b:
+            return False
+        # else:
+        #    #print(99)
+        #    return False
+        ''' I must be same size as Q '''
+        # I = np.eye(Q.shape[0])
+        # iq = I - Q
+        # F = np.linalg.pinv(iq)
+        # FR = F.T @ R
+        # print("FR", FR)
+        # except Exception as E:
+        #    print(E)
+        #    return False
         return True
-    return False
+    else:
+        return False
