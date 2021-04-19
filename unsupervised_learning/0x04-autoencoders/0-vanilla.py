71120adb36e7e06dda0e69f09ff445dd48f1f430
Ian Culp <icculp@gmail.com>
2021-04-19 23:31:38 +0000

    Reversing layers for decoder, latent in center of hidden layers

--- unsupervised_learning/0x04-autoencoders/0-vanilla.py
+++ unsupervised_learning/0x04-autoencoders/0-vanilla.py
@@ -23,16 +23,26 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     for hl in range(1, len(hidden_layers)):
         layer_enc = keras.layers.Dense(hidden_layers[hl],
                                        activation='relu')(layer_enc)
-    latent = keras.layers.Dense(latent_dims, activation='relu')(layer_enc)
-    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(latent)
-    auto = keras.Model(inputs, decoded)
-    auto.compile(optimizer='adam', loss='binary_crossentropy')
-
-    encoder = keras.Model(inputs, latent)
+    latent_enc = keras.layers.Dense(latent_dims, activation='relu')(layer_enc)
 
     encoded_input = keras.Input(shape=(latent_dims,))
-    decoder_layer = auto.layers[-1]
-    # keras.layers.Dense(latent_dims, activation='sigmoid')  # auto.layers[-1]
-    decoder = keras.Model(encoded_input, decoder_layer(encoded_input))
+    # print('hl first', hl)
+    latent = keras.layers.Dense(hidden_layers[hl],
+                                activation='relu')(encoded_input)
+    c = 1
+    for hl in range(len(hidden_layers) - 2, -1, -1):
+        # print('hl', hl)
+        decod = keras.layers.Dense(hidden_layers[hl],
+                                   activation='relu')(latent if c else decod)
+        c = 0
+    decoded = keras.layers.Dense(input_dims, activation='sigmoid')(decod)
 
+    encoder = keras.Model(inputs, latent_enc)
+    # decoder_layer = auto.layers[-1]
+    # keras.layers.Dense(latent_dims, activation='sigmoid')  # auto.layers[-1]
+    decoder = keras.Model(encoded_input, decoded)
+
+    outputs = decoder(encoder(inputs))
+    auto = keras.Model(inputs, outputs)
+    auto.compile(optimizer='adam', loss='binary_crossentropy')
     return encoder, decoder, auto
