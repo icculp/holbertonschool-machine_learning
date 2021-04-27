5745a950806d8c97264ca53e4ce300d6ec9dc74b
Ian Culp <icculp@gmail.com>
2021-04-27 16:45:09 +0000

    RNN cell complete

--- supervised_learning/0x0D-RNNs/0-rnn_cell copy.py
+++ supervised_learning/0x0D-RNNs/0-rnn_cell copy.py
@@ -26,7 +26,7 @@ class RNNCell:
     def softmax(z):
         ''' softmax activation function '''
         t = np.exp(z)
-        a = np.exp(z) / np.sum(t, axis=1).reshape(-1, 1)
+        a = np.exp(z) / np.sum(t, axis=1).reshape(-1,1)
         return a
 
     def forward(self, h_prev, x_t):
@@ -48,4 +48,48 @@ class RNNCell:
         # print('meow', cat.shape)
         h_next = np.tanh(cat @ self.Wh + self.bh)
         y = self.softmax(h_next @ self.Wy + self.by)
+        return h_next, y
+
+
+
+
+
+
+
+
+
+
+
+        m, i = x_t.shape
+        U = self.Wh[:i]
+        W = self.Wh[i:]
+        x = x_t
+        T = len(x_t)
+        # During forward propagation we save all hidden states in s because need them later.
+        # We add one additional element for the initial hidden, which we set to 0
+        s = np.zeros((T + 1, len(self.Wh[:self.Wh.shape[1]]) ))
+        s[-1] = np.zeros(self.Wh.shape[1])
+        # The outputs at each time step. Again, we save them for later.
+        o = np.zeros((T, len(self.Wh[:self.Wh.shape[1]])))
+        # For each time step...
+        for t in np.arange(T):
+            # Note that we are indxing U by x[t]. This is the same as multiplying U with a one-hot vector.
+            #s[t] = np.tanh(U[:, x_t[]] + W.dot(s[t - 1]))
+            o[t] = softmax(self.V.dot(s[t]))
+        return s, o
+        
+        m, i = x_t.shape
+        Wi = self.Wh[:i]
+        Wh = self.Wh[i:]
+        print("wi", Wi.shape, "wh", Wh.shape)
+        print("wh", self.Wh.shape, "wy", self.Wy.shape)
+        print("bh", self.bh.shape, "by", self.by.shape)
+        print("xtshape", x_t.shape, "hprev", h_prev.shape)
+        print("one", self.Wh[:i].shape)
+        one = self.Wy.dot(x_t)# np.dot(x_t, Wh)  # x_t.dot(self.Wh[:i])
+        two = h_prev @ Wh  # h_prev.dot(self.Wh[i:])
+        sum = one + two
+        h_next = np.tanh(sum + self.bh)
+        soft = h_next @ self.Wy
+        y = self.softmax(soft) # + self.by)
         return h_next, y
