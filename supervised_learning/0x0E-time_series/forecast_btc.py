82f96fbfec774aa20f6287f68593c60058e0341e
Ian Culp <icculp@gmail.com>
2021-05-07 00:22:15 +0000

    checking import fail

--- supervised_learning/0x0E-time_series/forecast_btc.py
+++ supervised_learning/0x0E-time_series/forecast_btc.py
@@ -2,3 +2,9 @@
 """
     Time Series Foreasting
 """
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+
+coinbase = pd.read_csv('coinbase.csv')
--- supervised_learning/0x0E-time_series/preprocess_data.py
+++ supervised_learning/0x0E-time_series/preprocess_data.py
@@ -5,6 +5,13 @@
 import pandas as pd
 
 
-def preprocess_data(data):
+def preprocess_data(data_fn, clean_fn):
     ''' preprocess time series csv '''
-    
+    # coinbase_fp = 'coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv'
+    df = pd.read_csv(data_fn)
+    clean = df.dropna(axis=0)
+    normal = (clean - clean.mean()) / clean.std()
+    normal['Timestamp'] = clean['Timestamp']
+    processed = normal[['Timestamp', 'Volume_(Currency)', 'Weighted_Price']]
+    processed.to_csv(clean_fn)
+    processed.head()
