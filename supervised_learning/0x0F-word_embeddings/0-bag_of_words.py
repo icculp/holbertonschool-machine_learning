38f7cbb44d99b651240f1bfe0984136d5aa5a7fd
Ian Culp <icculp@gmail.com>
2021-05-10 19:34:22 +0000

    Bag of words embedding matrix

--- supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -2,6 +2,8 @@
 """
     NLP - Word Embeddings
 """
+import numpy as np
+from sklearn.feature_extraction import text
 
 
 def bag_of_words(sentences, vocab=None):
@@ -14,4 +16,22 @@ def bag_of_words(sentences, vocab=None):
                 s is the number of sentences in sentences
                 f is the number of features analyzed
             features list of the features used for embeddings
+    """
+    # vocab = ['are', 'awesome', 'beautiful', 'cake']
+    Vectorizer = text.CountVectorizer(vocabulary=vocab)
+    embeddings = Vectorizer.fit_transform(sentences)
+    return embeddings.toarray(), Vectorizer.get_feature_names()
+    """ doing from scratch not necessary
+    features = set()
+    embeddings = [[0]]
+    for sentence in sentences:
+        # new = set(s.replace("'s","").strip('@!#$%^&*()\'\" ').\
+                lower() for s in sentence.split())
+        # print(new)
+        for n in new:
+            features.add(n)
+        #print(features)
+    features = list(features)
+    features.sort()
+    return embeddings, features
     """
