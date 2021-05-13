a88edf8705b134d85feac9e67cd987e90598b31d
Ian Culp <icculp@gmail.com>
2021-05-13 22:27:05 +0000

    pycodestyle

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -96,11 +96,13 @@ def bleu_score(original, machine_translated):
     print('LENGTH CLIPPED', len(clipped_precision_score))
     # print (clipped_precision_score)
 
-    weights = [.25,.25,.25,.25]
-    # weights = [1, 2.220446049250313e-16, 2.220446049250313e-16, 2.220446049250313e-16]
+    weights = [.25, .25, .25, .25]
+    # weights = [1, 2.220446049250313e-16,
+    #            2.220446049250313e-16, 2.220446049250313e-16]
     # weights = [.5, .5, 0, 0]
     # weights = [1, 0, 0, 0]
-    s = (w_i * np.log(p_i) for w_i, p_i in zip(weights, clipped_precision_score))
+    s = (w_i * np.log(p_i) for w_i, p_i in zip(weights,
+         clipped_precision_score))
     print(type(s))
     # for si in s:
     #    print(si)
@@ -109,98 +111,14 @@ def bleu_score(original, machine_translated):
     return s
 
 
-
-
-
-
 def modified_precision(references, hypothesis, n):
     """
     Calculate modified ngram precision.
-
-    The normal precision method may lead to some wrong translations with
-    high-precision, e.g., the translation, in which a word of reference
-    repeats several times, has very high precision.
-
-    This function only returns the Fraction object that contains the numerator
-    and denominator necessary to calculate the corpus-level precision.
-    To calculate the modified precision for a single pair of hypothesis and
-    references, cast the Fraction object into a float.
-
-    The famous "the the the ... " example shows that you can get BLEU precision
-    by duplicating high frequency words.
-
-        >>> reference1 = 'the cat is on the mat'.split()
-        >>> reference2 = 'there is a cat on the mat'.split()
-        >>> hypothesis1 = 'the the the the the the the'.split()
-        >>> references = [reference1, reference2]
-        >>> float(modified_precision(references, hypothesis1, n=1)) # doctest: +ELLIPSIS
-        0.2857...
-
-    In the modified n-gram precision, a reference word will be considered
-    exhausted after a matching hypothesis word is identified, e.g.
-
-        >>> reference1 = ['It', 'is', 'a', 'guide', 'to', 'action', 'that',
-        ...               'ensures', 'that', 'the', 'military', 'will',
-        ...               'forever', 'heed', 'Party', 'commands']
-        >>> reference2 = ['It', 'is', 'the', 'guiding', 'principle', 'which',
-        ...               'guarantees', 'the', 'military', 'forces', 'always',
-        ...               'being', 'under', 'the', 'command', 'of', 'the',
-        ...               'Party']
-        >>> reference3 = ['It', 'is', 'the', 'practical', 'guide', 'for', 'the',
-        ...               'army', 'always', 'to', 'heed', 'the', 'directions',
-        ...               'of', 'the', 'party']
-        >>> hypothesis = 'of the'.split()
-        >>> references = [reference1, reference2, reference3]
-        >>> float(modified_precision(references, hypothesis, n=1))
-        1.0
-        >>> float(modified_precision(references, hypothesis, n=2))
-        1.0
-
-    An example of a normal machine translation hypothesis:
-
-        >>> hypothesis1 = ['It', 'is', 'a', 'guide', 'to', 'action', 'which',
-        ...               'ensures', 'that', 'the', 'military', 'always',
-        ...               'obeys', 'the', 'commands', 'of', 'the', 'party']
-
-        >>> hypothesis2 = ['It', 'is', 'to', 'insure', 'the', 'troops',
-        ...               'forever', 'hearing', 'the', 'activity', 'guidebook',
-        ...               'that', 'party', 'direct']
-
-        >>> reference1 = ['It', 'is', 'a', 'guide', 'to', 'action', 'that',
-        ...               'ensures', 'that', 'the', 'military', 'will',
-        ...               'forever', 'heed', 'Party', 'commands']
-
-        >>> reference2 = ['It', 'is', 'the', 'guiding', 'principle', 'which',
-        ...               'guarantees', 'the', 'military', 'forces', 'always',
-        ...               'being', 'under', 'the', 'command', 'of', 'the',
-        ...               'Party']
-
-        >>> reference3 = ['It', 'is', 'the', 'practical', 'guide', 'for', 'the',
-        ...               'army', 'always', 'to', 'heed', 'the', 'directions',
-        ...               'of', 'the', 'party']
-        >>> references = [reference1, reference2, reference3]
-        >>> float(modified_precision(references, hypothesis1, n=1)) # doctest: +ELLIPSIS
-        0.9444...
-        >>> float(modified_precision(references, hypothesis2, n=1)) # doctest: +ELLIPSIS
-        0.5714...
-        >>> float(modified_precision(references, hypothesis1, n=2)) # doctest: +ELLIPSIS
-        0.5882352941176471
-        >>> float(modified_precision(references, hypothesis2, n=2)) # doctest: +ELLIPSIS
-        0.07692...
-
-
-    :param references: A list of reference translations.
-    :type references: list(list(str))
-    :param hypothesis: A hypothesis translation.
-    :type hypothesis: list(str)
-    :param n: The ngram order.
-    :type n: int
-    :return: BLEU's modified precision for the nth order ngram.
-    :rtype: Fraction
     """
     # Extracts all ngrams in hypothesis
     # Set an empty Counter if hypothesis is empty.
-    counts = Counter(ngrams(hypothesis, n)) if len(hypothesis) >= n else Counter()
+    counts = Counter(ngrams(hypothesis, n)) if\
+        len(hypothesis) >= n else Counter()
     # Extract a union of references' counts.
     # max_counts = reduce(or_, [Counter(ngrams(ref, n)) for ref in references])
     max_counts = {}
@@ -209,7 +127,8 @@ def modified_precision(references, hypothesis, n):
             Counter(ngrams(reference, n)) if len(reference) >= n else Counter()
         )
         for ngram in counts:
-            max_counts[ngram] = max(max_counts.get(ngram, 0), reference_counts[ngram])
+            max_counts[ngram] = max(max_counts.get(ngram, 0),
+                                    reference_counts[ngram])
 
     # Assigns the intersection between hypothesis and references' counts.
     clipped_counts = {
@@ -224,8 +143,6 @@ def modified_precision(references, hypothesis, n):
     return Fraction(numerator, denominator, _normalize=False)
 
 
-
-
 def uni_bleu(references, sentence):
     """ calculates the unigram BLEU score for a sentence:
         references is a list of reference translations
@@ -234,12 +151,12 @@ def uni_bleu(references, sentence):
         Returns: the unigram BLEU score
     """
     hypothesis = sentence
-    # weights=(0.25, 0.25, 0.25, 0.25)
-    weights=(1, 0, 0, 0)
-    # weights=(0, 1, 0, 0)
+    # weights = (0.25, 0.25, 0.25, 0.25)
+    weights = (1, 0, 0, 0)
+    # weights = (0, 1, 0, 0)
     # compute modified precision for 1-4 ngrams
-    p_numerators = Counter()  
-    p_denominators = Counter()  
+    p_numerators = Counter()
+    p_denominators = Counter()
     hyp_lengths, ref_lengths = 0, 0
 
     for i, _ in enumerate(weights, start=1):
@@ -247,15 +164,15 @@ def uni_bleu(references, sentence):
         p_numerators[i] += p_i.numerator
         p_denominators[i] += p_i.denominator
 
-    # compute brevity penalty    
+    # compute brevity penalty
     hyp_len = len(hypothesis)
     ref_len = closest_ref_length(references, hyp_len)
     bp = brevity_penalty(ref_len, hyp_len)
 
     # compute final score
     p_n = [
-        Fraction(p_numerators[i], p_denominators[i], 
-        _normalize=False)
+        Fraction(p_numerators[i], p_denominators[i],
+                 _normalize=False)
         for i, _ in enumerate(weights, start=1)
         if p_numerators[i] > 0
     ]
@@ -263,16 +180,3 @@ def uni_bleu(references, sentence):
     s = bp * math.exp(math.fsum(s))
 
     return s
-
-
-
-
-
-    l = len(references)
-    print('l', l)
-    score = 0
-    for i in range(l):
-        score += bleu_score(references[i], sentence)
-        print('score', score)
-    return score / 10#/ 4#bleu_score([references], [sentence]) #, weights=1)
-
