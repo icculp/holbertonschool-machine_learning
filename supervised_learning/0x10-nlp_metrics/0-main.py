254bd57d78422faab306da7f0e2c2520d1419b78
Ian Culp <icculp@gmail.com>
2021-05-14 00:52:51 +0000

    mains for troubleshooting imperfect modified precision

--- supervised_learning/0x10-nlp_metrics/0-main.py
+++ supervised_learning/0x10-nlp_metrics/0-main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
+import numpy as np
 uni_bleu = __import__('0-uni_bleu').uni_bleu
+from nltk.translate.bleu_score import corpus_bleu
+from nltk.translate.bleu_score import sentence_bleu
 
 references = [["the", "cat", "is", "on", "the", "mat"], ["there", "is", "a", "cat", "on", "the", "mat"]]
 sentence = ["there", "is", "a", "cat", "here"]
 
 print(uni_bleu(references, sentence))
+print(np.round(corpus_bleu([references], [sentence], weights=(1, 0, 0, 0)), 10))
+
+references = [["hello", "there", "my", "friend"]]
+sentence = ["hello", "hello", "there", "there", "friend"]
+print(np.round(uni_bleu(references, sentence), 10))
+print(np.round(corpus_bleu([references], [sentence], weights=(1, 0, 0, 0)), 10))
--- supervised_learning/0x10-nlp_metrics/2-main.py
+++ supervised_learning/0x10-nlp_metrics/2-main.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
+import numpy as np
 cumulative_bleu = __import__('2-cumulative_bleu').cumulative_bleu
+from nltk.translate.bleu_score import corpus_bleu
+from nltk.translate.bleu_score import sentence_bleu
 
 references = [["the", "cat", "is", "on", "the", "mat"], ["there", "is", "a", "cat", "on", "the", "mat"]]
 sentence = ["there", "is", "a", "cat", "here"]
 
 print(cumulative_bleu(references, sentence, 4))
+
+
+references = [["hello", "there", "my", "friend"]]
+sentence = ["hello", "hello", "there", "there", "friend"]
+print(np.round(cumulative_bleu(references, sentence, 2), 10))
+print(np.round(sentence_bleu(references, sentence, weights=(.5, .5, 0, 0)), 10))
