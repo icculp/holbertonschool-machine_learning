bfb6e6f6eb7d7e06861d0c793d9bdd0469986f65
Ian Culp <icculp@gmail.com>
2021-05-21 02:06:19 +0000

    positional encoding v2

--- supervised_learning/0x11-attention/4-positional_encoding.py
+++ supervised_learning/0x11-attention/4-positional_encoding.py
@@ -14,8 +14,14 @@ def positional_encoding(max_seq_len, dm):
     """
     pe = np.zeros((max_seq_len, dm))
 
+    pe = np.array([[pos / np.power(10000, 2 * (j // 2) / dm)
+                    for j in range(dm)]
+                  for pos in range(max_seq_len)])
+    pe[:, 0::2] = np.sin(pe[:, 0::2])
+    pe[:, 1::2] = np.cos(pe[:, 1::2])
+    '''
     for pos in range(max_seq_len):
         for i in range(0, dm, 2):
             pe[pos, i] = (np.sin(pos / (10000 ** ((2 * i) / dm))))
-            pe[pos, i + 1] = (np.cos(pos / (10000 ** ((2 * (i + 1)) / dm))))
+            pe[pos, i + 1] = (np.cos(pos / (10000 ** ((2 * (i + 1)) / dm))))'''
     return pe
