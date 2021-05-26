006ee4f014f4c649683f588e3739fc93b2bd748e
Ian Culp <icculp@gmail.com>
2021-05-26 01:17:26 +0000

    pycodestyle

--- supervised_learning/0x12-transformer_apps/0-dataset.py
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -2,11 +2,10 @@
 """
     Transformer Applications
 """
-import tensorflow.compat.v1 as tf
+import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
 
 
-tf.enable_v2_behavior()
 class Dataset():
     """ loads and preps a dataset for machine translation """
 
@@ -21,13 +20,17 @@ class Dataset():
             tokenizer_en is the English tokenizer created
                 from the training set
         """
-        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en', split='train', as_supervised=True)
-        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en', split='validation', as_supervised=True)
-        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.\
+            tokenize_dataset(self.data_train)
 
     def tokenize_dataset(self, data):
         """ creates sub-word tokenizers for our dataset:
-            data is a tf.data.Dataset whose examples are formatted as a tuple (pt, en)
+            data is a tf.data.Dataset whose examples are formatted as a tuple
+                (pt, en)
             pt is the tf.Tensor containing the Portuguese sentence
             en is the tf.Tensor containing the corresponding English sentence
             The maximum vocab size should be set to 2**15
@@ -35,8 +38,9 @@ class Dataset():
                 tokenizer_pt is the Portuguese tokenizer
                 tokenizer_en is the English tokenizer
         """
-        tokenizer_pt = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+        STE = tfds.deprecated.text.SubwordTextEncoder
+        tokenizer_pt = STE.build_from_corpus(
             (pt.numpy() for pt, en in data), target_vocab_size=2**15)
-        tokenizer_en = tfds.deprecated.text.SubwordTextEncoder.build_from_corpus(
+        tokenizer_en = STE.build_from_corpus(
             (en.numpy() for pt, en in data), target_vocab_size=2**15)
         return tokenizer_pt, tokenizer_en
--- supervised_learning/0x12-transformer_apps/0-main.py
+++ supervised_learning/0x12-transformer_apps/0-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-
-Dataset = __import__('0-dataset').Dataset
 import tensorflow as tf
+Dataset = __import__('0-dataset').Dataset
+
 
 data = Dataset()
 for pt, en in data.data_train.take(1):
