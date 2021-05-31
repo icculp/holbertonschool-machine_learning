d8dc6490adc4219562cd2c2e08d0573091fd4289
Ian Culp <icculp@gmail.com>
2021-05-31 15:03:25 -0500

    question answering and zendesk data

--- supervised_learning/0x13-qa_bot/0-qa.py
+++ supervised_learning/0x13-qa_bot/0-qa.py
@@ -12,5 +12,22 @@ def question_answer(question, reference):
         Returns: a string containing the answer
             If no answer is found, return None
     """
-    model = bert-uncased-tf2-qa
-    bert = BertTokenizer, bert-large-uncased-whole-word-masking-finetuned-squad
+    # tokenizer = BertTokenizer.from_pretrained('tokenizer_tf2_qa')
+    tokenizer = BertTokenizer.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    model = hub.load("https://tfhub.dev/see--/bert-uncased-tf2-qa/1")
+    # model = bert-uncased-tf2-qa
+    # bert = BertTokenizer, bert-large-uncased-whole-word-masking-finetuned-squad
+    q_tok = tokenizer.tokenize(question)
+    r_tok = tokenizer.tokenize(reference)
+    tokens = ['[CLS]'] + q_tok + ['[SEP]'] + r_tok + ['[SEP]']
+    input_word_ids = tokenizer.convert_tokens_to_ids(tokens)
+    input_mask = [1] * len(input_word_ids)
+    input_type_ids = [0] * (1 + len(q_tok) + 1) + [1] * (len(r_tok) + 1)
+    input_word_ids, input_mask, input_type_ids = map(lambda t: tf.expand_dims(
+        tf.convert_to_tensor(t, dtype=tf.int32), 0), (input_word_ids, input_mask, input_type_ids))
+    outputs = model([input_word_ids, input_mask, input_type_ids])
+    short_start = tf.argmax(outputs[0][0][1:]) + 1
+    short_end = tf.argmax(outputs[1][0][1:]) + 1
+    answer_tokens = tokens[short_start: short_end + 1]
+    answer = tokenizer.convert_tokens_to_string(answer_tokens)
+    return answer
