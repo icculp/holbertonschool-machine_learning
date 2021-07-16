9dbdc5d14c04578b07ea8f39402280c3008e12d5
Ian Culp <icculp@gmail.com>
2021-07-16 15:27:47 +0000

    policy gradient complete

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -20,3 +20,23 @@ def policy(matrix, weight):
         print('m', matrix.T[i])
         print('w', weight[i])
     return np.array(policy).sum(axis=0)'''
+
+
+def softmax_grad(softmax):
+    """ gradient of softmax """
+    s = softmax.reshape(-1, 1)
+    return np.diagflat(s) - np.dot(s, s.T)
+
+
+def policy_gradient(state, weight):
+    """ computes monte carlo policy gradient based on state and matrix
+        state: matrix representing the current observation of the environment
+        weight: matrix of random weight
+        Return: the action and the gradient (in this order)
+    """
+    probabilities = policy(state, weight)
+    action = np.random.choice(len(probabilities[0]), p=probabilities[0])
+    soft_der = softmax_grad(policy(state, weight))[action, :]
+    dlog = soft_der / probabilities[0, action]
+    grad = np.dot(state.T, dlog[None, :])
+    return action, grad
