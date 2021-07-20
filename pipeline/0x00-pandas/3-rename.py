0ba2bb51bb320c63b2d1598e5a3e9bf944ccb9b8
Ian Culp <icculp@gmail.com>
2021-07-20 15:17:49 +0000

    changing unit to second

--- pipeline/0x00-pandas/3-rename.py
+++ pipeline/0x00-pandas/3-rename.py
@@ -7,7 +7,7 @@ from_file = __import__('2-from_file').from_file
 df = from_file('coinbaseUSD_1-min_data_2014-12-01_to_2019-01-09.csv', ',')
 
 df.rename(columns={'Timestamp': 'Datetime'}, inplace=True)
-
+# df = df[::-1]
 df = df[['Datetime', 'Close']]
-df['Datetime'] = pd.to_datetime(df['Datetime'])
+df['Datetime'] = pd.to_datetime(df['Datetime'], unit='s')
 print(df.tail())
