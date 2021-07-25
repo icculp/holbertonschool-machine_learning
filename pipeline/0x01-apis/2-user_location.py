0b10b666749e9369377b93c6ad85d31e89077af5
Ian Culp <icculp@gmail.com>
2021-07-25 02:18:17 +0000

    rounding minutes

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -19,9 +19,9 @@ if __name__ == "__main__":
             reset_time = int(r.headers.get('X-Ratelimit-Reset'))
             now = time.time()
             minutes = reset_time - now
-            minutes = int(minutes / 60)
+            minutes = round(minutes / 60)
             # print(minutes)
-            print("Reset in {} minutes".format(minutes))
+            print("Reset in {} min".format(minutes))
             exit()
     user = r.json()
     #    i -= 1
@@ -30,35 +30,3 @@ if __name__ == "__main__":
         print(user.get('location'))
     else:
         print("Not found")
-
-
-def sentientPlanets():
-    """ returns the list of names of the home
-            planets of all sentient species
-    """
-    sentient_planets = []
-    species = []
-    url = 'https://swapi-api.hbtn.io/api/species/?format=json'
-    while 1:
-        # print(1)
-        r = requests.get(url).json()
-        species += r.get('results')
-        next = r.get('next')
-        if next:
-            url = next
-        else:
-            break
-    # print(species)
-    for specie in species:
-        # print('looping')
-        try:
-            if specie.get('designation') == 'sentient' or\
-                    specie.get('classification') == 'sentient':
-                planet_url = specie.get('homeworld')
-                if planet_url:
-                    name = requests.get(planet_url).json().get('name')
-                    sentient_planets.append(name)
-        except Exception as e:
-            print(e, specie.get('name'))
-            continue
-    return sentient_planets
