d1f5984fee1d981c49807e2344884d8161e6ae9b
Ian Culp <icculp@gmail.com>
2021-08-01 03:55:00 +0000

    safe div function

---
+++ pipeline/0x02-databases/21-div.sql
@@ -0,0 +1,16 @@
+-- valid_email
+DELIMITER //
+
+DROP FUNCTION IF EXISTS SafeDiv;
+
+CREATE FUNCTION SafeDiv (a INT, b INT)
+RETURNS FLOAT
+
+BEGIN
+    IF b = 0 THEN
+        RETURN 0;
+    END IF;
+    RETURN (a / b);
+END; //
+
+DELIMITER ;
