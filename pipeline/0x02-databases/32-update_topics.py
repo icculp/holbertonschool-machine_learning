84eab3a8090428adc9b9fb01750353eb36accabf
Ian Culp <icculp@gmail.com>
2021-08-01 23:48:50 +0000

    updating topics

---
+++ pipeline/0x02-databases/32-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 30-main """
+from pymongo import MongoClient
+
+
+def update_topics(mongo_collection, name, topics):
+    """ changes all topics of a school document based on the name:
+        mongo_collection will be the pymongo collection object
+        name (string) will be the school name to update
+        topics (list of strings) will be the list of topics approached in the school
+    """
+    mongo_collection.update({'name': name}, {'name': name, 'topics': topics})
+
