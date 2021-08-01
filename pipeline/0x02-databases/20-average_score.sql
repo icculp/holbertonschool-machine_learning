ca18e9f02c0e9b90ece65db334634970312ceb67
Ian Culp <icculp@gmail.com>
2021-08-01 03:39:08 +0000

    average score proc

---
+++ pipeline/0x02-databases/20-average_score.sql
@@ -0,0 +1,12 @@
+-- valid_email
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+CREATE PROCEDURE ComputeAverageScoreForUser (user_id INT)
+    BEGIN
+        SET @avg = (SELECT AVG(corrections.score) FROM corrections WHERE user_id = user_id);
+        UPDATE users SET average_score = @avg WHERE id = user_id;
+    END; //
+
+DELIMITER ;
