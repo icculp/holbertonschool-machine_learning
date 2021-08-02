a23e0fa4043f623a419f0aa92a11fed7cbf9176f
Ian Culp <icculp@gmail.com>
2021-08-02 18:17:15 +0000

    view with date search

---
+++ pipeline/0x02-databases/102-need_meeting.sql
@@ -0,0 +1,6 @@
+-- SQL COMMENT
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting
+AS 
+SELECT name FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR (last_meeting < ADDDATE(CURDATE(), INTERVAL -1 MONTH)));
