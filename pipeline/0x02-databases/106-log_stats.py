c2ab25c3a8165bbeb907554f0c9ec7326fd97de7
Ian Culp <icculp@gmail.com>
2021-08-03 02:33:12 +0000

    log statis with top ips

--- pipeline/0x02-databases/106-log_stats.py
+++ pipeline/0x02-databases/106-log_stats.py
@@ -7,6 +7,27 @@ if __name__ == '__main__':
     client = MongoClient('mongodb://127.0.0.1:27017')
     nginx = client.logs.nginx
     logs = nginx.find()
+    print(dir(logs))
+    #print(logs.collection)
+    ips = nginx.aggregate([
+                
+                {'$group': {'_id': '$ip', 'sum': { '$sum': 1}}},
+                {"$sort": {"sum": -1}},
+                {"$limit": 10}
+    ])
+    '''pipe = [{"$unwind": "$topics"},
+            {"$group":
+                {"_id": '$_id',
+                 "averageScore": {"$avg": "$topics.score"},
+                 "name": {'$first': '$name'}
+                 }
+             },
+            {"$sort": {"averageScore": -1}}
+            ]
+    logs.aggregate(pipe)'''
+    # ips = logs.count('ip')#.sort("ip", -1)#.limit(10)#logs.sort("ip", -1).limit(10)
+    #for document in logs: 
+    #    print(document.keys()) 
     get = nginx.find({'method': 'GET'})
     post = nginx.find({'method': 'POST'})
     put = nginx.find({'method': 'PUT'})
@@ -21,3 +42,9 @@ if __name__ == '__main__':
           "\tmethod PATCH: {}\n".format(patch.count()) +
           "\tmethod DELETE: {}".format(delete.count()))
     print("{} status check".format(status.count()))
+    print("IPs:")
+    for doc in ips:
+        print("\t{}: {}".format(doc.get('_id'), doc.get('sum')))
+#     +
+#          "{}".format(ips)
+#    )
